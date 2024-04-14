bb8bf47dd9efecba046b47be16f55daa2aa2d9ad
Cinquin Andy <andy.cinquin@infotel.com>
2024-04-14 21:02:54 +0200

    ✨ Update navigation links and add Next.js components
    Adjusted navigation links to Home, Features, Pricing, Documentation, and Contact. Added Link and Image components for better routing and image handling in the application. Improved user experience with these changes.

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,15 @@
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const navigation = [
-	{ name: 'Product', href: '#' },
-	{ name: 'Features', href: '#' },
-	{ name: 'Marketplace', href: '#' },
-	{ name: 'Company', href: '#' },
+	{ name: 'Home', href: '/' },
+	{ name: 'Features', href: '#features' },
+	{ name: 'Pricing', href: '#pricing' },
+	{ name: 'Documentation', href: '/doc' },
+	{ name: 'Contact', href: '/contact' },
 ]
 
 export default function Home() {
@@ -21,14 +24,16 @@ export default function Home() {
 					aria-label="Global"
 				>
 					<div className="flex lg:flex-1">
-						<a href="#" className="-m-1.5 p-1.5">
+						<Link href="#" className="-m-1.5 p-1.5">
 							<span className="sr-only">ForVoyez</span>
-							<img
+							<Image
 								className="h-8 w-auto"
-								src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+								src="/logo/logo.webp"
 								alt=""
+								width={80}
+								height={80}
 							/>
-						</a>
+						</Link>
 					</div>
 					<div className="flex lg:hidden">
 						<button
@@ -42,22 +47,22 @@ export default function Home() {
 					</div>
 					<div className="hidden lg:flex lg:gap-x-12">
 						{navigation.map(item => (
-							<a
+							<Link
 								key={item.name}
 								href={item.href}
 								className="text-sm font-semibold leading-6 text-gray-900"
 							>
 								{item.name}
-							</a>
+							</Link>
 						))}
 					</div>
 					<div className="hidden lg:flex lg:flex-1 lg:justify-end">
-						<a
+						<Link
 							href="#"
 							className="text-sm font-semibold leading-6 text-gray-900"
 						>
 							Log in <span aria-hidden="true">&rarr;</span>
-						</a>
+						</Link>
 					</div>
 				</nav>
 				<Dialog
@@ -69,14 +74,14 @@ export default function Home() {
 					<div className="fixed inset-0 z-50" />
 					<Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
 						<div className="flex items-center justify-between">
-							<a href="#" className="-m-1.5 p-1.5">
+							<Link href="#" className="-m-1.5 p-1.5">
 								<span className="sr-only">Your Company</span>
 								<img
 									className="h-8 w-auto"
 									src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
 									alt=""
 								/>
-							</a>
+							</Link>
 							<button
 								type="button"
 								className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -90,22 +95,22 @@ export default function Home() {
 							<div className="-my-6 divide-y divide-gray-500/10">
 								<div className="space-y-2 py-6">
 									{navigation.map(item => (
-										<a
+										<Link
 											key={item.name}
 											href={item.href}
 											className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
 										>
 											{item.name}
-										</a>
+										</Link>
 									))}
 								</div>
 								<div className="py-6">
-									<a
+									<Link
 										href="#"
 										className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
 									>
 										Log in
-									</a>
+									</Link>
 								</div>
 							</div>
 						</div>
@@ -159,29 +164,21 @@ export default function Home() {
 						<div className="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32">
 							<div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
 								<div className="relative w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
-									<h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-										We’re changing the way people connect.
+									<h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+										Generate Image Alt Text and Meta Descriptions in Seconds.
 									</h1>
 									<p className="mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
-										Cupidatat minim id magna ipsum sint dolor qui. Sunt sit in
-										quis cupidatat mollit aute velit. Et labore commodo nulla
-										aliqua proident mollit ullamco exercitation tempor. Sint
-										aliqua anim nulla sunt mollit id pariatur in voluptate
-										cillum.
+										Our AI-powered API makes it easy for developers to optimize
+										images for SEO. Save time, boost SEO, and streamline your
+										workflow with our powerful API.
 									</p>
 									<div className="mt-10 flex items-center gap-x-6">
-										<a
+										<Link
 											href="#"
 											className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 										>
 											Get started
-										</a>
-										<a
-											href="#"
-											className="text-sm font-semibold leading-6 text-gray-900"
-										>
-											Live demo <span aria-hidden="true">→</span>
-										</a>
+										</Link>
 									</div>
 								</div>
 								<div className="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
