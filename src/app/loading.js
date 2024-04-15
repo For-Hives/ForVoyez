dbd438fc108678c45deed6839824e0a6e04463d6
Cinquin Andy <andy.cinquin@infotel.com>
2024-04-15 19:21:44 +0200

    ✨ Add new images and corresponding webp files, remove loading component, update lang attribute in not-found page
    Added new images for abstract expressionist painting, futuristic city skyscrapers, minimalist interior modern living room, and tranquil forest crystal-clear. Removed the loading component file. Updated the lang attribute in the not-found page to include a language value of "en".

--- src/app/loading.js
+++
@@ -1,6 +0,0 @@
-import { LoaderComponent } from '@/components/Loaders/Loader.component'
-
-export default function Loading() {
-	// todo custom loading component with skeleton / base page
-	return <LoaderComponent></LoaderComponent>
-}
