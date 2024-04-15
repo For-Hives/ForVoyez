bb85879dcdedc2ad98804e6346b71712caf845ca
Cinquin Andy <andy.cinquin@infotel.com>
2024-04-15 19:29:59 +0200

    ✨ Add two new image components for the right side
    Added two new image components for the right side of the page, improving layout and design.

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,32 @@ import { ImageDemoLeft } from '@/app/imageDemoLeft'
 import { ImageDemoMidTop } from '@/app/imageDemoMidTop'
 import { ImageDemoMidBottom } from '@/app/imageDemoMidBottom'
 
+function ImageDemoRightTop() {
+	return (
+		<div className="relative">
+			<img
+				src="https://images.unsplash.com/photo-1670272504528-790c24957dda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=left&w=400&h=528&q=80"
+				alt=""
+				className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+			/>
+			<div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+		</div>
+	)
+}
+
+function ImageDemoRightBottom() {
+	return (
+		<div className="relative">
+			<img
+				src="https://images.unsplash.com/photo-1670272505284-8faba1c31f7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+				alt=""
+				className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+			/>
+			<div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+		</div>
+	)
+}
+
 export default function Home() {
 	return (
 		<div className="bg-white">
@@ -82,22 +108,8 @@ export default function Home() {
 										<ImageDemoMidBottom />
 									</div>
 									<div className="w-44 flex-none space-y-8 pt-32 sm:pt-0">
-										<div className="relative">
-											<img
-												src="https://images.unsplash.com/photo-1670272504528-790c24957dda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=left&w=400&h=528&q=80"
-												alt=""
-												className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
-											/>
-											<div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-										</div>
-										<div className="relative">
-											<img
-												src="https://images.unsplash.com/photo-1670272505284-8faba1c31f7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-												alt=""
-												className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
-											/>
-											<div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-										</div>
+										<ImageDemoRightTop />
+										<ImageDemoRightBottom />
 									</div>
 								</div>
 							</div>
