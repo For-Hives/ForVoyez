662bb69cb07100a19c02f34e088cd6381d16a2ec
Cinquin Andy <andy.cinquin@infotel.com>
2024-04-15 19:27:32 +0200

    ✨ Add new image components for different sections
    Added separate image components for the left, top middle, and bottom middle sections of the page to improve code organization and maintainability. Removed direct image elements in these sections.

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 'use client'
 import Link from 'next/link'
 import { NavbarComponent } from '@/components/Navbar.component'
-import Image from 'next/image'
+import { ImageDemoLeft } from '@/app/imageDemoLeft'
+import { ImageDemoMidTop } from '@/app/imageDemoMidTop'
+import { ImageDemoMidBottom } from '@/app/imageDemoMidBottom'
 
 export default function Home() {
 	return (
@@ -73,34 +75,11 @@ export default function Home() {
 								</div>
 								<div className="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
 									<div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
-										<div className="relative">
-											<Image
-												src="/images/abstract_expressionist_painting.webp"
-												alt="Image of an abstract expressionist painting, demo of the generated alt text API"
-												width={200}
-												height={350}
-												className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
-											/>
-											<div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-										</div>
+										<ImageDemoLeft />
 									</div>
 									<div className="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
-										<div className="relative">
-											<img
-												src="https://images.unsplash.com/photo-1485217988980-11786ced9454?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-												alt=""
-												className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
-											/>
-											<div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-										</div>
-										<div className="relative">
-											<img
-												src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=396&h=528&q=80"
-												alt=""
-												className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
-											/>
-											<div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
-										</div>
+										<ImageDemoMidTop />
+										<ImageDemoMidBottom />
 									</div>
 									<div className="w-44 flex-none space-y-8 pt-32 sm:pt-0">
 										<div className="relative">
