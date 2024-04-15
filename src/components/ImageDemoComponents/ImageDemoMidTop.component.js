29ce2b1f5eb8ab517619338ac306fc958322155e
Cinquin Andy <andy.cinquin@infotel.com>
2024-04-15 19:32:10 +0200

    ✨ Update image components imports and rename files
    Updated import paths for image components in the page file to match the renamed component files. Also, renamed image component files for consistency.

--- src/components/ImageDemoComponents/ImageDemoMidTop.component.js
+++ src/components/ImageDemoComponents/ImageDemoMidTop.component.js
@@ -1,8 +1,8 @@
-export function ImageDemoRightTop() {
+export function ImageDemoMidTopComponent() {
 	return (
 		<div className="relative">
 			<img
-				src="https://images.unsplash.com/photo-1670272504528-790c24957dda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=left&w=400&h=528&q=80"
+				src="https://images.unsplash.com/photo-1485217988980-11786ced9454?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
 				alt=""
 				className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
 			/>
