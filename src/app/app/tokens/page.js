3c6d7e977f7fa45d263e8a4b3f005461396a593b
breval <breval2000@live.fr>
2024-04-16 21:53:01 +0200

    chore(prisma): remove dev.db-journal file feat(tokens): add TokenCreate component for creating tokens feat(tokens): add createToken function for creating tokens using Prisma

--- src/app/app/tokens/page.js
+++ src/app/app/tokens/page.js
@@ -3,12 +3,11 @@ import { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import Navbar from '@/components/Navbar'
+import TokenCreate from '@/components/tokens/TokenCreate'
 import { auth } from '@clerk/nextjs'
 
 export default function TokenPage() {
 	const [tokens, setTokens] = useState([])
-	const [newTokenName, setNewTokenName] = useState('')
-	const [newTokenExpiry, setNewTokenExpiry] = useState('')
 	const [isModalOpen, setIsModalOpen] = useState(false)
 	const [modalContent, setModalContent] = useState('')
 	const [modalAction, setModalAction] = useState(() => () => {})
@@ -23,31 +22,6 @@ export default function TokenPage() {
 		setModalAction(() => action)
 	}
 
-	async function createToken() {
-		'use server'
-
-		const { userId } = auth()
-
-		if (!userId) {
-			throw new Error('You must be logged to create a token')
-		}
-
-		if (!newTokenExpiry || new Date(newTokenExpiry) < new Date()) {
-			// todo : penser a check que la date d'expiration est bien utiliser
-			showToast('Please provide a valid expiry date in the future.')
-			return
-		}
-		const newToken = {
-			name: newTokenName,
-			createdAt: new Date().toISOString(),
-			expiredAt: new Date(newTokenExpiry).toISOString(),
-		}
-		setTokens([...tokens, newToken])
-		setNewTokenName('')
-		setNewTokenExpiry('')
-		showToast('Token created successfully!')
-	}
-
 	const deleteToken = index => {
 		const updatedTokens = tokens.filter((_, i) => i !== index)
 		setTokens(updatedTokens)
@@ -60,41 +34,22 @@ export default function TokenPage() {
 			<div className="p-8">
 				<h1 className="mb-4 text-xl font-bold">Token Management</h1>
 				<div className="flex flex-wrap">
-					{tokens.map((token, index) => (
-						<div key={index} className="mb-2 mr-2 flex flex-col border p-4">
-							<p>Name: {token.name}</p>
-							<p>Created at: {token.createdAt}</p>
-							<p>Expires at: {token.expiredAt}</p>
-							<button
-								onClick={() => handleModal(() => deleteToken(index))}
-								className="mt-2 rounded bg-red-500 px-4 py-2 text-white"
-							>
-								Delete
-							</button>
-						</div>
-					))}
+					{tokens &&
+						tokens.map((token, index) => (
+							<div key={index} className="mb-2 mr-2 flex flex-col border p-4">
+								<p>Name: {token.name}</p>
+								<p>Created at: {token.createdAt}</p>
+								<p>Expires at: {token.expiredAt}</p>
+								<button
+									onClick={() => handleModal(() => deleteToken(index))}
+									className="mt-2 rounded bg-red-500 px-4 py-2 text-white"
+								>
+									Delete
+								</button>
+							</div>
+						))}
 				</div>
-				<div className="mb-4">
-					<input
-						type="text"
-						placeholder="New token name"
-						value={newTokenName}
-						onChange={e => setNewTokenName(e.target.value)}
-						className="mr-2 border p-2"
-					/>
-					<input
-						type="date"
-						value={newTokenExpiry}
-						onChange={e => setNewTokenExpiry(e.target.value)}
-						className="mr-2 border p-2"
-					/>
-					<button
-						onClick={createToken}
-						className="rounded bg-blue-500 px-4 py-2 text-white"
-					>
-						Create Token
-					</button>
-				</div>
+				<TokenCreate tokens={tokens} setTokens={setTokens} />
 				{isModalOpen && (
 					<div className="fixed inset-0 flex items-center justify-center bg-gray-600 bg-opacity-50">
 						<div className="bg-white p-8">
---
+++ src/components/tokens/TokenCreate.js
@@ -0,0 +1,55 @@
+import { auth } from '@clerk/nextjs'
+import { useState } from 'react'
+import { createToken } from '@/components/tokens/TokensCRUD'
+
+export default function TokenCreate({ tokens, setTokens }) {
+	const [newTokenName, setNewTokenName] = useState('')
+	const [newTokenExpiry, setNewTokenExpiry] = useState('')
+
+	async function create() {
+		if (!newTokenExpiry || new Date(newTokenExpiry) < new Date()) {
+			// todo : penser a check que la date d'expiration est bien utiliser
+			showToast('Please provide a valid expiry date in the future.')
+			return
+		}
+
+		const newToken = {
+			name: newTokenName,
+			createdAt: new Date().toISOString(),
+			expiredAt: new Date(newTokenExpiry).toISOString(),
+		}
+
+		console.log(newToken)
+
+		const result = await createToken(newToken)
+
+		setTokens([...tokens, newToken])
+		setNewTokenName('')
+		setNewTokenExpiry('')
+		showToast('Token created successfully!')
+	}
+
+	return (
+		<div className="mb-4">
+			<input
+				type="text"
+				placeholder="New token name"
+				value={newTokenName}
+				onChange={e => setNewTokenName(e.target.value)}
+				className="mr-2 border p-2"
+			/>
+			<input
+				type="date"
+				value={newTokenExpiry}
+				onChange={e => setNewTokenExpiry(e.target.value)}
+				className="mr-2 border p-2"
+			/>
+			<button
+				onClick={create}
+				className="rounded bg-blue-500 px-4 py-2 text-white"
+			>
+				Create Token
+			</button>
+		</div>
+	)
+}
