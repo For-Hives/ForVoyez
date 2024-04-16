3db477d2521023aa3a80cdd04cf89d496c2d367c
Cinquin Andy <andy.cinquin@infotel.com>
2024-04-16 19:24:25 +0200

    center sign in & sign up

--- src/app/sign-up/[[...sign-up]]/page.js
+++ src/app/sign-up/[[...sign-up]]/page.js
@@ -1,5 +1,9 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-	return <SignUp />
+	return (
+		<main className={'flex h-[100dvh] w-[100dvw] items-center justify-center'}>
+			<SignUp />
+		</main>
+	)
 }
