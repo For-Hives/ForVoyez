d2761500e3f440c1accf934390a345d9b881b63e
Cinquin Andy <andy.cinquin@infotel.com>
2024-04-16 19:48:34 +0200

    🛠️ Update : responsive & hover on images
    - Added state for initialization in GlassmorphismComponent
    - Implemented delayed opacity change on div
    - Updated cursor animation styles
    - Added new URLs in NavbarComponent
    - Adjusted global CSS properties

--- src/components/ImageDemoComponents/Glassmorphism.component.js
+++ src/components/ImageDemoComponents/Glassmorphism.component.js
@@ -1,6 +1,10 @@
+'use client'
 import { TypeAnimation } from 'react-type-animation'
+import { useEffect, useState } from 'react'
 
 export function GlassmorphismComponent({ title, alt, caption }) {
+	const [initialized, setInitialized] = useState(false)
+
 	function truncateText(text, maxLength) {
 		if (text.length > maxLength) {
 			return text.substring(0, maxLength - 3) + '...'
@@ -8,34 +12,44 @@ export function GlassmorphismComponent({ title, alt, caption }) {
 		return text
 	}
 
+	useEffect(() => {
+		// 	after 3.5s, disable 'opacity-0' class on the div
+		setTimeout(() => {
+			setInitialized(true)
+		}, 3500)
+	}, [])
+
 	return (
-		<div
-			className={
-				'shadow-[0 4px 30px rgba(0, 0, 0, 0.1)] glassmorphism-animation absolute left-0 top-0 h-full w-full rounded-lg bg-white/75 backdrop-blur-lg ' +
-				'z-50 border border-white/75 hover:opacity-0'
-			}
-		>
+		<div>
 			<div
 				className={
-					'pointer-events-none flex h-full w-full scale-[90%] flex-col items-start text-xs leading-4'
+					`${initialized ? '!opacity-100 hover:!opacity-0' : '!opacity-0'} shadow-[0 4px 30px rgba(0, 0, 0, 0.1)] glassmorphism-animation absolute 
+					left-0 top-0 h-full w-full rounded-lg bg-white/75 backdrop-blur-lg transition-all ` +
+					'z-50 border border-white/75'
 				}
 			>
-				<p>{`{`}</p>
-				<div className={'flex flex-grow items-start'}>
-					<TypeAnimation
-						style={{
-							whiteSpace: 'pre-wrap',
-						}}
-						sequence={[
-							3500,
-							`\n\t"title":"${truncateText(title, 50)}",\n\n\t"alternativeText": "${truncateText(alt, 125)}",\n\n\t"caption":"${truncateText(caption, 75)}"\n`,
-						]}
-						repeat={0}
-						speed={90}
-						cursor={false}
-					/>
+				<div
+					className={
+						'flex h-full w-full scale-[90%] flex-col items-start text-xs leading-4'
+					}
+				>
+					<p>{`{`}</p>
+					<div className={'flex flex-grow items-start'}>
+						<TypeAnimation
+							style={{
+								whiteSpace: 'pre-wrap',
+							}}
+							sequence={[
+								3500,
+								`\n\t"title":"${truncateText(title, 50)}",\n\n\t"alternativeText": "${truncateText(alt, 125)}",\n\n\t"caption":"${truncateText(caption, 75)}"\n`,
+							]}
+							repeat={0}
+							speed={90}
+							cursor={false}
+						/>
+					</div>
+					<p>{`}`}</p>
 				</div>
-				<p>{`}`}</p>
 			</div>
 		</div>
 	)
