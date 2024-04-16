421f9b1b857d4fd79a268ea536ba8a3cac76859a
Cinquin Andy <andy.cinquin@infotel.com>
2024-04-16 13:31:58 +0200

    ✨ Update styling for ImageDemoLeftComponent
    Added a new nested div with text content and adjusted z-index for elements to improve layout structure and visual hierarchy.

--- src/components/ImageDemoComponents/ImageDemoLeft.component.js
+++ src/components/ImageDemoComponents/ImageDemoLeft.component.js
@@ -3,14 +3,30 @@ import Image from 'next/image'
 export function ImageDemoLeftComponent() {
 	return (
 		<div className="relative">
+			<div
+				className={
+					'shadow-[0 4px 30px rgba(0, 0, 0, 0.1)] absolute left-0 top-0 h-full w-full rounded-xl bg-white/50 backdrop-blur-lg ' +
+					'z-50 border border-white/30'
+				}
+			>
+				<div className={'flex h-full w-full scale-[90%] flex-col items-start'}>
+					<p>{`{`}</p>
+					<div className={'flex flex-grow items-center'}>
+						&emsp;
+						{`"alternativeText": "Chocolate cake toffee croissant shortbread
+							sesame snaps danish .sesame snaps danish .sesame snaps danish ."`}
+					</div>
+					<p>{`}`}</p>
+				</div>
+			</div>
 			<Image
 				src="/images/abstract_expressionist_painting.webp"
 				alt="Image of an abstract expressionist painting, demo of the generated alt text API"
 				width={200}
 				height={350}
-				className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
+				className="z-40 aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
 			/>
-			<div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+			<div className="pointer-events-none absolute inset-0 z-40 rounded-xl ring-1 ring-inset ring-gray-900/10" />
 		</div>
 	)
 }
