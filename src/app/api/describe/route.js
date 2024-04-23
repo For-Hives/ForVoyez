e9af475ef38ceebaad6c8227e426b6cfa9e28f2d
breval <breval2000@live.fr>
2024-04-23 16:37:34 +0200

    chore(describe.bru): update auth type to bearer in describe endpoint feat(describe.bru): add bearer authentication with token verification feat(route.js): add token verification and credit check for POST request feat(TokenList.js): refactor TokenList component to use arrow functions and template literals
    fix(jwt.service.js): fix indentation and formatting issues
    feat(jwt.service.js): update verifyJwt function to use hardcoded issuer and audience values

--- src/app/api/describe/route.js
+++ src/app/api/describe/route.js
@@ -2,10 +2,47 @@ import {
 	blobToBase64,
 	getImageDescription,
 } from '@/services/imageDescription.service'
+import { verifyJwt } from '@/services/jwt.service'
+import { prisma } from '@/services/prisma.service'
 
 export async function POST(request) {
 	// Process multipart/form-data containing an image and a JSON schema.
 	try {
+		// get the authorisation header
+		const authorization = request.headers.get('Authorization')
+		if (!authorization) {
+			return new Response('Unauthorized', {
+				status: 401,
+				statusText: 'Unauthorized, missing Authorization header',
+			})
+		}
+
+		// check if the token is valid
+		try {
+			let payload = await verifyJwt(authorization)
+			console.log(payload)
+
+			// check if the token is still valid and if the user is have credit left
+			let user = await prisma.user.findUnique({
+				where: {
+					clerkId: payload.userId,
+				},
+			})
+
+			console.log(user)
+			if (user.credits <= 0) {
+				return new Response('Unauthorized, no credit left', {
+					status: 401,
+					statusText: 'Unauthorized',
+				})
+			}
+		} catch (error) {
+			return new Response('Unauthorized, invalid token', {
+				status: 401,
+				statusText: 'Unauthorized',
+			})
+		}
+
 		const formData = await request.formData()
 
 		const file = formData.get('image')
