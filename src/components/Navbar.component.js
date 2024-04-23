deeac55e169a1ef297f21d46f410c27adfc4791e
Cinquin Andy <andy.cinquin@infotel.com>
2024-04-23 18:31:36 +0200

    ✨ Update Navbar logo rendering
    Adjust the logo in the Navbar component to use a new image file format and specify dimensions for better display.

--- src/components/Navbar.component.js
+++ src/components/Navbar.component.js
@@ -86,11 +86,13 @@ export function NavbarComponent() {
 				<Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
 					<div className="flex items-center justify-between">
 						<Link href="#" className="-m-1.5 p-1.5">
-							<span className="sr-only">Your Company</span>
-							<img
+							<span className="sr-only">ForVoyez</span>
+							<Image
 								className="h-8 w-auto"
-								src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+								src="/logo/logo.webp"
 								alt=""
+								width={80}
+								height={80}
 							/>
 						</Link>
 						<button
