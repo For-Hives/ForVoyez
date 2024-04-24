fbbbf019652daa9f59260b313717f95ac0297b1f
CinquinAndy <cinquin.andy@gmail.com>
2024-04-24 16:58:54 +0200

    🚀 Add grid layout for features section
    Updated the layout of the Features section to use a grid structure for better organization and readability. Also, made adjustments to display AI-Powered Image Metadata prominently.

--- src/components/LandingComponents/Features/Feature.component.js
+++ src/components/LandingComponents/Features/Feature.component.js
@@ -57,53 +57,54 @@ export function FeatureComponent() {
 	}, [])
 
 	return (
-		<div className="bg-white py-24 sm:py-32">
+		<div className="overflow-hidden bg-white py-24 sm:py-32">
 			<div className="mx-auto max-w-7xl px-6 lg:px-8">
-				<div className="mx-auto max-w-2xl sm:text-center">
-					<h2 className="text-base font-semibold leading-7 text-[#ff6545]">
-						Everything you need
-					</h2>
-					<p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-						Automate your image metadata generation
-					</p>
-					<p className="mt-6 text-lg leading-8 text-gray-600">
-						Our AI-powered tool simplifies the process of generating
-						SEO-optimized image metadata, saving you time and effort while
-						boosting your online visibility.
-					</p>
-				</div>
-			</div>
-			<div className="relative overflow-hidden pt-16">
-				<div className="mx-auto max-w-7xl px-6 lg:px-8">
-					{isRiveLoaded && (
-						<RiveComponent
-							src="/animation_features/landing_art_forvoyez.riv"
-							className="h-full min-h-[500px] w-full min-w-[5vw]"
-							autoPlay={true}
-							stateMachines={'State Machine 1'}
-						/>
-					)}
-					<div className="relative" aria-hidden="true">
-						<div className="absolute -inset-x-20 bottom-0 pt-[7%]" />
-					</div>
-				</div>
-			</div>
-			<div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
-				<dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
-					{features.map(feature => (
-						<div key={feature.name} className="relative pl-9">
-							<dt className="inline font-semibold text-gray-900">
-								<feature.icon
-									className="absolute left-1 top-1 h-5 w-5 text-[#ff6545]"
-									aria-hidden="true"
-								/>
-								{feature.name}
-							</dt>
-							<br />
-							<dd className="inline">{feature.description}</dd>
+				<div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+					<div className="lg:pr-8 lg:pt-4">
+						<div className="lg:max-w-lg">
+							<h2 className="text-base font-semibold leading-7 text-[#ff6545]">
+								AI-Powered Image Metadata
+							</h2>
+							<p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+								Optimize Your Images Effortlessly
+							</p>
+							<p className="mt-6 text-lg leading-8 text-gray-600">
+								Our AI-powered tool simplifies the process of generating
+								SEO-optimized image metadata, saving you time and effort while
+								boosting your online visibility.
+							</p>
+							<dl className="mt-16 max-w-xl space-y-12 text-base leading-7 text-gray-600 lg:max-w-none">
+								{features.map(feature => (
+									<div key={feature.name} className="relative pl-9">
+										<dt className="inline font-semibold text-gray-900">
+											<feature.icon
+												className="absolute left-1 top-1 h-5 w-5 text-[#ff6545]"
+												aria-hidden="true"
+											/>
+											{feature.name}
+										</dt>
+										<br />
+										<dd className="inline">{feature.description}</dd>
+									</div>
+								))}
+							</dl>
 						</div>
-					))}
-				</dl>
+					</div>
+					{isRiveLoaded && (
+						<div
+							className={
+								'flex w-[48rem] max-w-none items-center justify-center rounded-xl sm:w-[57rem] md:-ml-4 lg:-ml-0'
+							}
+						>
+							<RiveComponent
+								src="/animation_features/landing_art_forvoyez.riv"
+								className="min-h-[50vh] w-full sm:min-h-[60vh]"
+								autoPlay={true}
+								stateMachines={'State Machine 1'}
+							/>
+						</div>
+					)}
+				</div>
 			</div>
 		</div>
 	)
