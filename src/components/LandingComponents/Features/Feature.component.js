eb507a9849936ba8d2cce9392a23c949710020d0
Cinquin Andy <andy.cinquin@infotel.com>
2024-04-24 21:04:27 +0200

    ✨ Update RiveComponent import and adjust container styling
    Changed the import statement for RiveComponent to fix an issue. Also, modified the container styling by adding a 'relative' class.

--- src/components/LandingComponents/Features/Feature.component.js
+++ src/components/LandingComponents/Features/Feature.component.js
@@ -8,7 +8,7 @@ import {
 	SparklesIcon,
 } from '@heroicons/react/20/solid'
 import { useEffect, useState } from 'react'
-import { RiveComponent } from '@rive-app/react-canvas'
+import RiveComponent from '@rive-app/react-canvas'
 
 const features = [
 	{
@@ -57,7 +57,7 @@ export function FeatureComponent() {
 	}, [])
 
 	return (
-		<div className="overflow-hidden bg-white py-24 sm:py-32">
+		<div className="relative overflow-hidden py-24 sm:py-32">
 			<div className="mx-auto max-w-7xl px-6 lg:px-8">
 				<div className="relative mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
 					<div className="lg:pr-8 lg:pt-4">
