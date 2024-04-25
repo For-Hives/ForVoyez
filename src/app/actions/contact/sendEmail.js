049eeaee62689f6ada09a57d60cd1c6542122e5c
Cinquin Andy <andy.cinquin@infotel.com>
2024-04-25 18:59:25 +0200

    ✨ Add 'use server' directive and implement email sending functionality
    Added 'use server' directive at the beginning of the file. Implemented a function to send emails with detailed content including sender's info and message. Error handling included.

--- src/app/actions/contact/sendEmail.js
+++ src/app/actions/contact/sendEmail.js
@@ -1,3 +1,4 @@
+'use server'
 const formData = require('form-data')
 const Mailgun = require('mailgun.js')
 const mailgun = new Mailgun(formData)
@@ -5,3 +6,35 @@ const mg = mailgun.client({
 	username: 'api',
 	key: process.env.MAILGUN_API_KEY,
 })
+
+export async function sendEmail(formData) {
+	const firstName = formData.get('first-name')
+	const lastName = formData.get('last-name')
+	const company = formData.get('company')
+	const email = formData.get('email')
+	const phone = formData.get('phone-number')
+	const subject = formData.get('subject')
+	const message = formData.get('message')
+
+	try {
+		await mg.messages.create(process.env.MAILGUN_DOMAIN, {
+			from: 'ForVoyez <noreply@forvoyez.fr>',
+			to: 'contact@forvoyez.fr',
+			subject: `New contact message - ${subject}`,
+			text: `
+				Prénom: ${firstName}
+				Nom: ${lastName}
+				Entreprise: ${company}
+				Email: ${email}
+				Téléphone: ${phone}
+				
+				Message:
+				${message}`,
+		})
+
+		return { success: true }
+	} catch (error) {
+		console.error('Error sending email:', error)
+		return { success: false }
+	}
+}
