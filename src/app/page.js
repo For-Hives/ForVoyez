514741af5783175d46c72956106c85bddfcb67f2
Cinquin Andy <andy.cinquin@infotel.com>
2024-04-25 14:36:38 +0200

    ✨ Add PricingComponent to Home page
    Include PricingComponent in the Home page rendering. This change adds the PricingComponent alongside existing components like Navbar, Hero, and Feature on the Home page.

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,16 @@
 import { NavbarComponent } from '@/components/Navbar.component'
 import { HeroComponent } from '@/components/LandingComponents/Hero/Hero.component'
 import { FeatureComponent } from '@/components/LandingComponents/Features/Feature.component'
+import { PricingComponent } from '@/components/LandingComponents/Pricing/Pricing.component'
 
 export default function Home() {
 	return (
-		<div className="min-h-[700vh] bg-white">
+		<div className="bg-white">
 			<NavbarComponent />
 			<main>
 				<HeroComponent />
 				<FeatureComponent />
+				<PricingComponent />
 			</main>
 		</div>
 	)
