df3d9ea3c66da6b719c6b9c2e41467375f07fee2
Cinquin Andy <andy.cinquin@infotel.com>
2024-04-25 18:59:04 +0200

    ✨ Add functionality for sending emails
    Introduce code to handle email sending using Mailgun API.

---
+++ src/app/actions/contact/sendEmail.js
@@ -0,0 +1,7 @@
+const formData = require('form-data')
+const Mailgun = require('mailgun.js')
+const mailgun = new Mailgun(formData)
+const mg = mailgun.client({
+	username: 'api',
+	key: process.env.MAILGUN_API_KEY,
+})
