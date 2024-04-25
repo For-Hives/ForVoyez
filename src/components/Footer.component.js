1692a74469fbe2007c5c1c7a45af5420556a41ad
Cinquin Andy <andy.cinquin@infotel.com>
2024-04-25 17:06:21 +0200

    ✨ Refactor footer component and CTA section
    Adjusted footer layout for responsiveness and updated CTA content. Made changes to improve visual consistency and user experience.

--- src/components/Footer.component.js
+++ src/components/Footer.component.js
@@ -81,47 +81,45 @@ const navigation = {
 
 export function FooterComponent() {
 	return (
-		<footer className="bg-white">
-			<div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
-				<div className="mb-10 flex justify-center">
-					<Image
-						width={50}
-						height={50}
-						src="/logo/logo.webp"
-						alt="ForVoyez Logo"
-					/>
-				</div>
-				<nav
-					className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12"
-					aria-label="Footer"
-				>
-					{navigation.main.map(item => (
-						<div key={item.name} className="pb-6">
-							<Link
-								href={item.href}
-								className="text-sm leading-6 text-gray-600 hover:text-[#ff6545]"
-							>
-								{item.name}
-							</Link>
-						</div>
-					))}
-				</nav>
-				<div className="mt-10 flex justify-center space-x-10">
-					{navigation.social.map(item => (
-						<Link
-							key={item.name}
-							href={item.href}
-							className="text-gray-400 hover:text-[#ff6545]"
-						>
-							<span className="sr-only">{item.name}</span>
-							<item.icon className="h-6 w-6" aria-hidden="true" />
-						</Link>
-					))}
-				</div>
-				<p className="mt-10 text-center text-xs leading-5 text-gray-500">
-					&copy; {new Date().getFullYear()} ForVoyez. All rights reserved.
-				</p>
+		<footer className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:pb-20 sm:pt-32 lg:px-8">
+			<div className="mb-10 flex justify-center">
+				<Image
+					width={50}
+					height={50}
+					src="/logo/logo.webp"
+					alt="ForVoyez Logo"
+				/>
 			</div>
+			<nav
+				className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12"
+				aria-label="Footer"
+			>
+				{navigation.main.map(item => (
+					<div key={item.name} className="pb-6">
+						<Link
+							href={item.href}
+							className="text-sm leading-6 text-gray-600 hover:text-[#ff6545]"
+						>
+							{item.name}
+						</Link>
+					</div>
+				))}
+			</nav>
+			<div className="mt-10 flex justify-center space-x-10">
+				{navigation.social.map(item => (
+					<Link
+						key={item.name}
+						href={item.href}
+						className="text-gray-400 hover:text-[#ff6545]"
+					>
+						<span className="sr-only">{item.name}</span>
+						<item.icon className="h-6 w-6" aria-hidden="true" />
+					</Link>
+				))}
+			</div>
+			<p className="mt-10 text-center text-xs leading-5 text-gray-500">
+				&copy; {new Date().getFullYear()} ForVoyez. All rights reserved.
+			</p>
 		</footer>
 	)
 }
