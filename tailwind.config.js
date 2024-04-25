794eb173cda415c86dbdb96735a15fab49cecc8b
Cinquin Andy <andy.cinquin@infotel.com>
2024-04-25 17:44:29 +0200

    ✨ Add new Tailwind CSS plugin for forms
    Include a new Tailwind CSS plugin for forms in the configuration file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,5 +21,5 @@ module.exports = {
 		},
 	},
 	darkMode: 'class',
-	plugins: [require('@tailwindcss/typography')],
+	plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 }
