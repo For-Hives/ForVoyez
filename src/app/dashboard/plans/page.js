4ad0818856dab21dc7402ca3d48ecfcff9681ca9
breval <breval2000@live.fr>
2024-04-26 20:39:55 +0200

    refactor(plans/page.js): update import paths and functions for plans page feat(plans/page.js): display plans fetched from database and allow subscription

--- src/app/dashboard/plans/page.js
+++ src/app/dashboard/plans/page.js
@@ -1,33 +1,28 @@
 'use client'
 
-import {
-	getAllProductsVariants,
-	getCheckoutURL,
-	listProducts,
-} from '@/services/lemonsqueezy.service'
 import { useEffect, useState } from 'react'
+import { getPlans } from '@/services/database.service' // Ajuste le chemin selon ton architecture
 import Image from 'next/image'
-// import {redirect} from 'next/navigation'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import { syncPlans } from '@/services/database.service'
+import { getCheckoutURL } from '@/services/lemonsqueezy.service'
 
 export default function PlansPage() {
 	const router = useRouter()
 
-	const [products, setProducts] = useState([])
+	const [plans, setPlans] = useState([])
 
 	useEffect(() => {
-		listProducts().then(r => setProducts(r))
+		getPlans().then(setPlans)
 	}, [])
 
-	console.table(products)
+	console.table(plans)
 
 	async function subscribe(variantId) {
 		try {
-			syncPlans().then(r => console.log(r))
-			// const url = await getCheckoutURL(variantId)
-			// await router.push(url)
+			// syncPlans().then(r => console.log(r))
+			const url = await getCheckoutURL(variantId)
+			await router.push(url)
 		} catch (e) {
 			console.log(e)
 		}
@@ -35,59 +30,41 @@ export default function PlansPage() {
 
 	return (
 		<>
-			<h1>Plans </h1>
+			<h1 className="my-6 text-center text-2xl font-bold">Available Plans</h1>
 
-			{products &&
-				products.map((product, index) => {
-					return (
-						<div key={index} className="mb-2 mr-2 flex flex-col border p-4">
-							<p>Name: {product.attributes.name}</p>
-							<p>Descriptions: {product.attributes.description}</p>
-							<p>Created at: {product.attributes.created_at.toString()}</p>
-							<Image
-								alt={product.attributes.description}
-								src={product.attributes.large_thumb_url}
-								width={250}
-								height={250}
-							/>
-							<p>Price : {product.attributes.price_formatted}</p>
-							<p>test mode ? : {product.attributes.test_mode}</p>
-
-							{/* todo : verifier si il a pas deja souscris */}
-
+			<div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+				{plans.map((plan, index) => (
+					<div
+						key={index}
+						className="max-w-sm overflow-hidden rounded p-4 shadow-lg"
+					>
+						<Image
+							alt="Plan Image" // Assure-toi que les plans ont des images ou ajuste cette partie
+							src="/path/to/your/image.jpg"
+							width={250}
+							height={250}
+							className="w-full"
+						/>
+						<div className="px-6 py-4">
+							<div className="mb-2 text-xl font-bold">{plan.name}</div>
+							<p className="text-base text-gray-700">
+								{plan.description || 'No description available.'}
+							</p>
+						</div>
+						<div className="px-6 pb-2 pt-4">
+							<span className="mb-2 mr-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+								Price: €{plan.price / 100}/month
+							</span>
 							<button
-								className="mt-2 rounded bg-red-500 px-4 py-2 text-white"
-								onClick={async () => {
-									subscribe(product.relationships.variants.data[0].id)
-								}}
+								onClick={() => subscribe(plan.id)}
+								className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
 							>
-								buy
+								Subscribe
 							</button>
 						</div>
-					)
-				})}
-
-			{/*{*/}
-			{/*    "store_id": 84282,*/}
-			{/*    "name": "ForVoyez API to extract meta-description from images",*/}
-			{/*    "slug": "forvoyez-api-to-extract-meta-description-from-images",*/}
-			{/*    "description": "<p>ForVoyez API to extract meta-description from images</p>",*/}
-			{/*    "status": "published",*/}
-			{/*    "status_formatted": "Published",*/}
-			{/*    "thumb_url": "https://lemonsqueezy.imgix.net/media/84282/7b96a385-fb1f-4a8a-8b88-97c3ae2eed14.png?fit=clip&h=100&ixlib=php-3.3.1&w=100&s=c110b47fedab3a6864eb74ef0c4f17de",*/}
-			{/*    "large_thumb_url": "https://lemonsqueezy.imgix.net/media/84282/7b96a385-fb1f-4a8a-8b88-97c3ae2eed14.png?fit=clip&h=1000&ixlib=php-3.3.1&w=1000&s=48887f11ca206a5c389c0e95481316ff",*/}
-			{/*    "price": 1000,*/}
-			{/*    "price_formatted": "€10.00/month",*/}
-			{/*    "from_price": null,*/}
-			{/*    "to_price": null,*/}
-			{/*    "pay_what_you_want": false,*/}
-			{/*    "buy_now_url": "https://forvoyez.lemonsqueezy.com/checkout/buy/866eb225-d7f2-4638-af9b-ae9b8cbc9754",*/}
-			{/*    "from_price_formatted": null,*/}
-			{/*    "to_price_formatted": null,*/}
-			{/*    "created_at": "2024-04-23T15:41:43.000000Z",*/}
-			{/*    "updated_at": "2024-04-23T15:49:33.000000Z",*/}
-			{/*    "test_mode": true*/}
-			{/*}*/}
+					</div>
+				))}
+			</div>
 		</>
 	)
 }
