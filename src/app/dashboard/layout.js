c3dfd4ebe1c41c3e1d43fe1ea531bc7c6de2160e
Cinquin Andy <andy.cinquin@infotel.com>
2024-04-28 23:02:10 +0200

    ✨ Improve layout styling for better responsiveness
    Adjust the div class to ensure full height display, enhancing responsiveness.

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -3,7 +3,7 @@ import { LayoutDashboard } from '@/app/dashboard/LayoutDashboard'
 export default async function Layout({ children }) {
 	return (
 		<div className="flex min-h-full bg-white antialiased">
-			<div className="w-full">
+			<div className="h-full w-full">
 				<LayoutDashboard>{children}</LayoutDashboard>
 			</div>
 		</div>
