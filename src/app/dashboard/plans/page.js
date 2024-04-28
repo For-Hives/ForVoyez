c4445a3779bfd560e6cb6f4cc1b0a98749447ed2
breval <breval2000@live.fr>
2024-04-28 20:40:46 +0200

    fix(database.service.js): refactor nsyncPlans function to syncPlans and add filter parameter to getPlans function

--- src/app/dashboard/plans/page.js
+++ src/app/dashboard/plans/page.js
@@ -11,6 +11,7 @@ export default function PlansPage() {
 	const router = useRouter()
 
 	const [plans, setPlans] = useState([])
+	const [isMonthly, setIsMonthly] = useState(true)
 
 	useEffect(() => {
 		getPlans().then(setPlans)
@@ -32,38 +33,66 @@ export default function PlansPage() {
 		<>
 			<h1 className="my-6 text-center text-2xl font-bold">Available Plans</h1>
 
-			<div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-				{plans.map((plan, index) => (
-					<div
-						key={index}
-						className="max-w-sm overflow-hidden rounded p-4 shadow-lg"
-					>
-						<Image
-							alt="Plan Image" // Assure-toi que les plans ont des images ou ajuste cette partie
-							src="/path/to/your/image.jpg"
-							width={250}
-							height={250}
-							className="w-full"
-						/>
-						<div className="px-6 py-4">
-							<div className="mb-2 text-xl font-bold">{plan.name}</div>
-							<p className="text-base text-gray-700">
-								{plan.description || 'No description available.'}
-							</p>
+			{/* switch from monthly to yearly */}
+			<div className="flex justify-center">
+				<button
+					onClick={() => setIsMonthly(true)}
+					className={`${
+						isMonthly ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'
+					} rounded-l px-4 py-2 font-bold focus:outline-none`}
+				>
+					Monthly
+				</button>
+				<button
+					onClick={() => setIsMonthly(false)}
+					className={`${
+						!isMonthly ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'
+					} rounded-r px-4 py-2 font-bold focus:outline-none`}
+				>
+					Yearly
+				</button>
+			</div>
+
+			{/* Plans */}
+
+			{/* show the plan nicely on the page in the middle with flex */}
+			<div className="mt-6 flex flex-wrap justify-center">
+				{plans.map((plan, index) => {
+					if (isMonthly && plan.billingCycle === 'year') return null
+					if (!isMonthly && plan.billingCycle === 'month') return null
+
+					return (
+						<div
+							key={index}
+							className="max-w-sm overflow-hidden rounded p-4 shadow-lg"
+						>
+							<Image
+								alt="Plan Image" // Assure-toi que les plans ont des images ou ajuste cette partie
+								src="/path/to/your/image.jpg"
+								width={250}
+								height={250}
+								className="w-full"
+							/>
+							<div className="px-6 py-4">
+								<div className="mb-2 text-xl font-bold">{plan.name}</div>
+								<p className="text-base text-gray-700">
+									{plan.description || 'No description available.'}
+								</p>
+							</div>
+							<div className="px-6 pb-2 pt-4">
+								<span className="mb-2 mr-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+									Price: €{plan.price / 100}/month
+								</span>
+								<button
+									onClick={() => subscribe(plan.id)}
+									className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+								>
+									Subscribe
+								</button>
+							</div>
 						</div>
-						<div className="px-6 pb-2 pt-4">
-							<span className="mb-2 mr-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
-								Price: €{plan.price / 100}/month
-							</span>
-							<button
-								onClick={() => subscribe(plan.id)}
-								className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
-							>
-								Subscribe
-							</button>
-						</div>
-					</div>
-				))}
+					)
+				})}
 			</div>
 		</>
 	)
--- src/services/database.service.js
+++ src/services/database.service.js
@@ -8,82 +8,13 @@ import {
 import { prisma } from '@/services/prisma.service'
 import { getProduct } from '@lemonsqueezy/lemonsqueezy.js'
 
-export async function nsyncPlans() {
-	console.log('Synchronisation des plans')
-
-	try {
-		const products = await listProducts()
-		console.log('Liste des produits obtenue')
-
-		for (const product of products) {
-			console.log('Traitement du produit :', product.id)
-			const variants = await listVariants(product.id)
-			console.log('Variantes du produit obtenues')
-
-			for (const variant of variants) {
-				console.log('Traitement de la variante :', variant.id)
-				const prices = await listPrice(variant.id)
-				console.log('Prix de la variante obtenus')
-				// check if the status is "published"
-				if (variant.attributes.status !== 'published') {
-					console.log('Variante non publiée, on passe à la suivante')
-					continue
-				}
-
-				// the first price is the newest one
-				// we only need to sync the first price
-
-				for (const price of prices) {
-					console.log('Traitement du prix :', price)
-
-					// log all data
-					console.log('product', product)
-					console.log('variant', variant)
-					console.log('price:', price)
-
-					let planData = {
-						productId: product.id,
-						variantId: variant.id,
-						name: variant.attributes.name,
-						description: variant.attributes.description,
-						price: price.attributes.unit_price,
-						credits: price.attributes.package_size || 0,
-						billingCycle: price.attributes.renewal_interval_unit || 'NA',
-						category: price.attributes.category,
-						pricingScheme: price.attributes.scheme,
-						setupFeeEnabled: price.attributes.setup_fee_enabled || false,
-						setupFee: price.attributes.setup_fee,
-						packageSize: price.attributes.package_size,
-						tiers: JSON.stringify(price.attributes.tiers),
-						renewalIntervalUnit: price.attributes.renewal_interval_unit,
-						renewalIntervalCount: price.attributes.renewal_interval_quantity,
-						trialIntervalUnit: price.attributes.trial_interval_unit,
-						trialIntervalCount: price.attributes.trial_interval_quantity,
-					}
-
-					try {
-						const createdPlan = await prisma.plan.upsert({
-							where: { variantId: variant.id },
-							update: planData,
-							create: planData,
-						})
-						console.log('Plan créé ou mis à jour :', createdPlan)
-					} catch (e) {
-						console.error(
-							'Erreur lors de la création ou de la mise à jour du plan :',
-							e
-						)
-					}
-				}
-			}
-		}
-	} catch (e) {
-		console.error('Erreur dans la synchronisation des plans :', e)
-	}
-}
-
-export async function getPlans() {
+export async function getPlans(filter = null) {
 	const plans = await prisma.plan.findMany()
+
+	// filter can be "yearly" or "monthly"
+	if (filter) {
+		return plans.filter(plan => plan.billingCycle === filter)
+	}
 
 	syncPlans()
 
