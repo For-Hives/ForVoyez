a11bac41f3f569f502d67d35b131a1335367f614
Cinquin Andy <andy.cinquin@infotel.com>
2024-04-28 20:18:31 +0200

    ✨ Add header navigation with mobile responsiveness
    Imported necessary components for header navigation and implemented responsive design for mobile devices. Included links to website, documentation, and profile pages.

---
+++ src/app/dashboard/HeaderDashoard.js
@@ -0,0 +1,83 @@
+import { forwardRef } from 'react'
+import Link from 'next/link'
+import clsx from 'clsx'
+import { motion, useScroll, useTransform } from 'framer-motion'
+
+import Image from 'next/image'
+import {
+	MobileNavigationDashboard,
+	useIsInsideMobileNavigation,
+	useMobileNavigationStore,
+} from '@/app/dashboard/MobileNavigationDashboard'
+
+function TopLevelNavItem({ href, children }) {
+	return (
+		<li>
+			<Link
+				href={href}
+				className="text-sm leading-5 text-slate-600 transition hover:text-slate-900"
+			>
+				{children}
+			</Link>
+		</li>
+	)
+}
+
+export const HeaderDashboard = forwardRef(function HeaderDashboard(
+	{ className },
+	ref
+) {
+	let { isOpen: mobileNavIsOpen } = useMobileNavigationStore()
+	let isInsideMobileNavigation = useIsInsideMobileNavigation()
+
+	let { scrollY } = useScroll()
+	let bgOpacityLight = useTransform(scrollY, [0, 72], [0.5, 0.9])
+
+	return (
+		<motion.div
+			ref={ref}
+			className={clsx(
+				className,
+				'fixed inset-x-0 top-0 z-50 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80',
+				!isInsideMobileNavigation && 'backdrop-blur-sm lg:left-72 xl:left-80',
+				isInsideMobileNavigation
+					? 'bg-white'
+					: 'bg-white/[var(--bg-opacity-light)]'
+			)}
+			style={{
+				'--bg-opacity-light': bgOpacityLight,
+			}}
+		>
+			<div
+				className={clsx(
+					'absolute inset-x-0 top-full h-px transition',
+					(isInsideMobileNavigation || !mobileNavIsOpen) && 'bg-slate-900/7.5'
+				)}
+			/>
+			<div className="flex items-center gap-5 lg:hidden">
+				<MobileNavigationDashboard />
+				<Link href="/" aria-label="Home">
+					<span className="sr-only">ForVoyez</span>
+					<Image
+						className="h-8 w-auto"
+						src="/logo/logo.webp"
+						alt="logo ForVoyez"
+						width={80}
+						height={80}
+					/>
+				</Link>
+			</div>
+			<div className="flex items-center gap-5">
+				<nav className="hidden md:block">
+					<ul role="list" className="flex items-center gap-8">
+						<TopLevelNavItem href="https://forvoyez.fr">
+							Website
+						</TopLevelNavItem>
+						<TopLevelNavItem href="/docs">Documentation</TopLevelNavItem>
+						<TopLevelNavItem href="/profile">Profile</TopLevelNavItem>
+					</ul>
+				</nav>
+			</div>
+		</motion.div>
+	)
+})
