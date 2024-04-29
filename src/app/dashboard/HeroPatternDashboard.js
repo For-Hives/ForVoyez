d38c879a6a1792147233363dc84b353d40e1d8d2
Cinquin Andy <andy.cinquin@infotel.com>
2024-04-29 23:01:51 +0200

    ✨ Update pointer-events style in HeroPatternDashboard
    Adjust the CSS class to include 'pointer-events-none' for overlay.

--- src/app/dashboard/HeroPatternDashboard.js
+++ src/app/dashboard/HeroPatternDashboard.js
@@ -2,7 +2,7 @@ import { GridPatternDashboard } from '@/app/dashboard/GridPatternDashboard'
 
 export function HeroPatternDashboard() {
 	return (
-		<div className="absolute inset-0 -z-10 mx-0 max-w-none overflow-hidden">
+		<div className="pointer-events-none absolute inset-0 z-10 mx-0 max-w-none overflow-hidden">
 			<div className="absolute left-1/2 top-0 ml-[-38rem] h-[25rem] w-[81.25rem]">
 				<div className="absolute inset-0 bg-gradient-to-r from-forvoyez_orange-300 to-white opacity-40 [mask-image:radial-gradient(farthest-side_at_top,white,transparent)]">
 					<GridPatternDashboard
