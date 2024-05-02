7dc124e7ca2fae11316dce84d5f768e103b8c10d
Cinquin Andy <andy.cinquin@infotel.com>
2024-05-02 17:03:09 +0200

    ✨ Add SectionProviderAccountComponent to LayoutAccountComponent
    Added a new SectionProviderAccountComponent to better structure the account layout. Updated rendering of user information and navigation components accordingly.

--- src/components/AccountComponents/LayoutAccount.component.js
+++ src/components/AccountComponents/LayoutAccount.component.js
@@ -8,60 +8,63 @@ import { HeaderDashboard } from '@/components/AccountComponents/HeaderAccount.co
 import { NavigationAccountComponent } from '@/components/AccountComponents/NavigationAccount.component'
 import { UserButton, useUser } from '@clerk/nextjs'
 import { HeroPatternAccountComponent } from '@/components/AccountComponents/HeroPatternAccount.component'
+import { SectionProviderAccountComponent } from '@/components/AccountComponents/SectionProviderAccount.component'
 
 export function LayoutAccountComponent({ children }) {
 	const { user } = useUser()
 
 	return (
-		<div className="h-full lg:ml-72 xl:ml-80">
-			<motion.header
-				layoutScroll
-				className="contents h-full lg:pointer-events-none lg:fixed lg:inset-0 lg:z-40 lg:flex lg:gap-4"
-			>
-				<div className="contents h-full lg:pointer-events-auto lg:flex lg:w-72 lg:flex-col lg:justify-between lg:overflow-y-auto lg:border-r lg:border-slate-900/10 lg:px-6 lg:pb-8 lg:pt-4 xl:w-80">
-					<div className={'flex flex-col'}>
-						<div className="hidden lg:flex">
-							<Link href="/" aria-label="Home">
-								<span className="sr-only">ForVoyez</span>
-								<Image
-									className="h-8 w-auto"
-									src="/logo/logo.webp"
-									alt="logo ForVoyez"
-									width={80}
-									height={80}
+		<SectionProviderAccountComponent sections={[]}>
+			<div className="h-full lg:ml-72 xl:ml-80">
+				<motion.header
+					layoutScroll
+					className="contents h-full lg:pointer-events-none lg:fixed lg:inset-0 lg:z-40 lg:flex lg:gap-4"
+				>
+					<div className="contents h-full lg:pointer-events-auto lg:flex lg:w-72 lg:flex-col lg:justify-between lg:overflow-y-auto lg:border-r lg:border-slate-900/10 lg:px-6 lg:pb-8 lg:pt-4 xl:w-80">
+						<div className={'flex flex-col'}>
+							<div className="hidden lg:flex">
+								<Link href="/" aria-label="Home">
+									<span className="sr-only">ForVoyez</span>
+									<Image
+										className="h-8 w-auto"
+										src="/logo/logo.webp"
+										alt="logo ForVoyez"
+										width={80}
+										height={80}
+									/>
+								</Link>
+							</div>
+							<HeaderDashboard />
+							<NavigationAccountComponent className="hidden h-full lg:mt-10 lg:block" />
+						</div>
+						{user && (
+							<div className={'hidden items-center gap-2 lg:flex'}>
+								<UserButton
+									appearance="ghost"
+									userProfileMode="navigation"
+									userProfileUrl="/profile"
+									afterSignOutUrl="/"
 								/>
-							</Link>
-						</div>
-						<HeaderDashboard />
-						<NavigationAccountComponent className="hidden h-full lg:mt-10 lg:block" />
+								<Link href="/profile" className={'h-full w-full'}>
+									<span className="text-sm font-medium text-slate-900">
+										{user.firstName} {user.lastName}
+									</span>
+								</Link>
+							</div>
+						)}
 					</div>
-					{user && (
-						<div className={'hidden items-center gap-2 lg:flex'}>
-							<UserButton
-								appearance="ghost"
-								userProfileMode="navigation"
-								userProfileUrl="/profile"
-								afterSignOutUrl="/"
-							/>
-							<Link href="/profile" className={'h-full w-full'}>
-								<span className="text-sm font-medium text-gray-900">
-									{user.firstName} {user.lastName}
-								</span>
-							</Link>
+				</motion.header>
+				<div className="relative flex h-full flex-col px-4 pt-14 sm:px-6 lg:px-8">
+					<main className="flex-auto">
+						<HeroPatternAccountComponent />
+						<div className="flex h-full flex-col pb-10 pt-16">
+							<div className={'prose z-20 mx-auto max-w-5xl flex-auto'}>
+								{children}
+							</div>
 						</div>
-					)}
+					</main>
 				</div>
-			</motion.header>
-			<div className="relative flex h-full flex-col px-4 pt-14 sm:px-6 lg:px-8">
-				<main className="flex-auto">
-					<HeroPatternAccountComponent />
-					<div className="flex h-full flex-col pb-10 pt-16">
-						<div className={'prose z-20 mx-auto max-w-5xl flex-auto'}>
-							{children}
-						</div>
-					</div>
-				</main>
 			</div>
-		</div>
+		</SectionProviderAccountComponent>
 	)
 }
