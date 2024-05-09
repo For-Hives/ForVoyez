fdd45f023a4d65513ab955aaa4aa012305ace677
Breval <breval2000@live.fr>
2024-05-09 15:55:13 +0200

    optimize import

--- src/app/(dashboard)/app/usage/page.js
+++ src/app/(dashboard)/app/usage/page.js
@@ -1,13 +1,6 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
-import { getPlans } from '@/services/database.service' // Ajuste le chemin selon ton architecture
-import Image from 'next/image'
-import Link from 'next/link'
-import { useRouter } from 'next/navigation'
-import { getCheckoutURL } from '@/services/lemonsqueezy.service'
-import { PricingComponent } from '@/components/Landing/Pricing/Pricing.component'
-import { ChangingPlansComponent } from '@/components/dashboard/ChangingPlans.component'
+import React from 'react'
 import { UsageChartComponent } from '@/components/usage/usageChart.component'
 
 export default function PlansPage() {
