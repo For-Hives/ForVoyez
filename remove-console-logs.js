46b4363a25540de021cc1f16f88581c867689bf4
Cinquin Andy <cinquin.andy@gmail.com>
2024-05-10 11:17:26 +0200

    Update: Improve console log removal - Refactored filter condition to remove all console methods - Enhanced code readability by simplifying the filter logic
    The change was made to enhance the accuracy of removing console logs and simplify the filtering process for better code maintenance.

--- remove-console-logs.js
+++ remove-console-logs.js
@@ -4,7 +4,9 @@ const path = require('path')
 function removeConsoleLogs(filePath) {
 	const fileContent = fs.readFileSync(filePath, 'utf-8')
 	const lines = fileContent.split('\n')
-	const filteredLines = lines.filter(line => !line.includes('console.log'))
+	// Remove lines that contain console.[smtg]
+	// const filteredLines = lines.filter(line => !line.includes('console.log'))
+	const filteredLines = lines.filter(line => !line.includes('console.'))
 
 	if (lines.length !== filteredLines.length) {
 		fs.writeFileSync(filePath, filteredLines.join('\n'), 'utf-8')
