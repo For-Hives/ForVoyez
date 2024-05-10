cdc6230c8bff23315d95818d2cf562fe08ae4e46
Breval <breval2000@live.fr>
2024-05-10 11:34:50 +0200

    display nothing if usage empty

--- src/components/usage/usageChart.component.js
+++ src/components/usage/usageChart.component.js
@@ -28,40 +28,53 @@ export function UsageChartComponent() {
 		<div className="bg-white py-24 sm:py-32">
 			<div className="mx-auto max-w-7xl px-6 lg:px-8">
 				<div className="mx-auto max-w-4xl text-center" id={'usage'}>
+					<div className="">
+						How much left
+						{usage && usage.length ? (
+							<p>
+								<span>{usage.length > 0 ? usage[usage.length - 1].y : 0}</span>
+								/100
+							</p>
+						) : (
+							<p>no usage yet</p>
+						)}
+					</div>
 					<h2 className="text-base font-semibold leading-7 text-forvoyez_orange-500">
 						Usage
 					</h2>
 					<p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-						Follow your usage in real-time
+						Follow how much you use the API in real-time
 					</p>
 				</div>
 				<div className="h-[50vh] min-h-[500px]">
-					<ResponsiveContainer width="100%" height="100%">
-						<AreaChart
-							width={500}
-							height={400}
-							data={usage}
-							margin={{
-								top: 10,
-								right: 30,
-								left: 0,
-								bottom: 0,
-							}}
-						>
-							<CartesianGrid strokeDasharray="3 3" />
-							<XAxis dataKey="x" />
-							<YAxis />
-							<Tooltip />
-							<Area
-								type="monotone"
-								dataKey="y"
-								stroke="#fff"
-								fill="#ff6545"
-								dot={true}
-								fillOpacity={1}
-							/>
-						</AreaChart>
-					</ResponsiveContainer>
+					{usage && usage.length && (
+						<ResponsiveContainer width="100%" height="100%">
+							<AreaChart
+								width={500}
+								height={400}
+								data={usage}
+								margin={{
+									top: 10,
+									right: 30,
+									left: 0,
+									bottom: 0,
+								}}
+							>
+								<CartesianGrid strokeDasharray="3 3" />
+								<XAxis dataKey="x" />
+								<YAxis />
+								<Tooltip />
+								<Area
+									type="monotone"
+									dataKey="y"
+									stroke="#fff"
+									fill="#ff6545"
+									dot={true}
+									fillOpacity={1}
+								/>
+							</AreaChart>
+						</ResponsiveContainer>
+					)}{' '}
 				</div>
 			</div>
 		</div>
