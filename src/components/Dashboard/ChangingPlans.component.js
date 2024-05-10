7ffd8ef4157c978e95c6273c212f3426ceb70da2
Cinquin Andy <cinquin.andy@gmail.com>
2024-05-10 14:59:44 +0200

    Update: Improve tier description rendering and pricing format - Added dangerous set HTML to render tier descriptions safely - Updated price display to include 2 decimal places and proper currency symbol
    These changes were made to enhance the user experience by accurately displaying tier information and pricing details.

--- src/components/Dashboard/ChangingPlans.component.js
+++ src/components/Dashboard/ChangingPlans.component.js
@@ -317,7 +317,7 @@ export function ChangingPlansComponent() {
 						</div>
 						<ul className="mt-8 space-y-3 text-sm leading-6 text-slate-600 xl:mt-10">
 							{JSON.parse(
-								'["100 credits/month", "Basic metadata generation", "Community support", "Accept classic image formats, (JPEG, PNG, WEBP)", "Full HD image support, (up to 1080p)"]'
+								'["All Growth plan features","Unlimited credits","Advanced metadata generation","24/7 dedicated support","Custom SLAs","Volume discounts","Access to beta features","Priority access to new features","Priority access to the playground","Dedicated hosting option","Deep integration with existing systems"]'
 							).map(feature => (
 								<li key={feature} className="flex gap-x-3">
 									<CheckIcon
