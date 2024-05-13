1a1137fa569960bd064e5e7a7be0e4ac6f60bb81
Cinquin Andy <andy.cinquin@infotel.com>
2024-05-13 20:08:49 +0200

    🚀 Refactor mobile image and text components
    - Updated component names for clarity and consistency
    - Reorganized import statements for better readability

--- src/components/Landing/Hero/AnimateSideHeroMobile.component.js
+++ src/components/Landing/Hero/AnimateSideHeroMobile.component.js
@@ -1,11 +1,10 @@
 'use client'
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
-import { ImageDemoMidBottomComponent } from '@/components/Landing/Hero/ImageDemoComponents/mobile/ImageDemoMidBottom.component'
-import { ImageDemoMidTopComponent } from '@/components/Landing/Hero/ImageDemoComponents/mobile/ImageDemoMidTop.component'
-import { ImageDemoRightBottomComponent } from '@/components/Landing/Hero/ImageDemoComponents/mobile/ImageDemoRightBottom.component'
-import { ImageDemoRightTopComponent } from '@/components/Landing/Hero/ImageDemoComponents/mobile/ImageDemoRightTop.component'
+import { ImageDemoBottomMobileComponent } from '@/components/Landing/Hero/ImageDemoComponents/mobile/ImageDemoBottomMobileComponent'
+import { ImageDemoTopMobileComponent } from '@/components/Landing/Hero/ImageDemoComponents/mobile/ImageDemoTopMobileComponent'
+import { TextDemoBottomMobileComponent } from '@/components/Landing/Hero/ImageDemoComponents/mobile/TextDemoBottomMobileComponent'
+import { TextDemoTopMobileComponent } from '@/components/Landing/Hero/ImageDemoComponents/mobile/TextDemoTopMobileComponent'
 
 export function AnimateSideHeroMobileComponent() {
 	const [textInButton, setTextInButton] = useState('Generate Now!')
@@ -14,12 +13,12 @@ export function AnimateSideHeroMobileComponent() {
 		<div className="relative mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
 			{/* View mobile */}
 			<div className="mr-auto block w-44 flex-none space-y-8 sm:mr-0 sm:hidden sm:pt-52 lg:pt-36">
-				<ImageDemoMidTopComponent />
-				<ImageDemoMidBottomComponent />
+				<ImageDemoTopMobileComponent />
+				<ImageDemoBottomMobileComponent />
 			</div>
 			<div className="block w-44 flex-none space-y-8 pt-32 sm:hidden sm:pt-0">
-				<ImageDemoRightTopComponent />
-				<ImageDemoRightBottomComponent />
+				<TextDemoTopMobileComponent />
+				<TextDemoBottomMobileComponent />
 			</div>
 		</div>
 	)
