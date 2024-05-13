b0f87d94f0220cfa417ddfd325e5acc162aa4c2f
Cinquin Andy <cinquin.andy@gmail.com>
2024-05-13 15:15:54 +0200

    Link to https://doc.forvoyez.com/

--- src/components/Navbar.component.js
+++ src/components/Navbar.component.js
@@ -11,7 +11,7 @@ const navigation = [
 	{ name: 'Home', href: '/' },
 	{ name: 'Features', href: '/#features' },
 	{ name: 'Pricing', href: '/#pricing' },
-	{ name: 'Documentation', href: '/documentation' },
+	{ name: 'Documentation', href: 'https://doc.forvoyez.com/' },
 	{ name: 'Contact', href: '/contact' },
 ]
 
