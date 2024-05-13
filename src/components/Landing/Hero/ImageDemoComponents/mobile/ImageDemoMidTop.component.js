c9d9f6d9758f042b584794e2091fa5a97ea79023
Cinquin Andy <andy.cinquin@infotel.com>
2024-05-13 19:27:30 +0200

    ✨ Add mobile hero animation components
    Added new mobile hero animation components and updated existing desktop components for better user interaction and visual appeal. Included animations for button clicks and text changes to enhance user experience.

---
+++ src/components/Landing/Hero/ImageDemoComponents/mobile/ImageDemoMidTop.component.js
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+
+import { GlassmorphismComponent } from '@/components/Landing/Hero/ImageDemoComponents/Glassmorphism.component'
+
+export function ImageDemoMidTopComponent() {
+	return (
+		<div className="relative">
+			<GlassmorphismComponent
+				title={'The City of Tomorrow'}
+				alt={
+					'A futuristic city with towering skyscrapers, sleek flying vehicles, and holographic billboards. The streets are illuminated by neon lights, and advanced robots assist humans in their daily lives.'
+				}
+				caption={
+					'Step into the city of tomorrow, where technology and humanity intertwine in a breathtaking urban landscape.'
+				}
+			/>
+			<Image
+				src="/images/futuristic_city_skyscrapers.webp"
+				alt="Image of an abstract expressionist painting, demo of the generated alt text API"
+				width={200}
+				height={350}
+				className="aspect-[2/3] w-full rounded-xl bg-slate-900/5 object-cover shadow-lg"
+			/>
+			<div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-slate-900/10" />
+		</div>
+	)
+}
