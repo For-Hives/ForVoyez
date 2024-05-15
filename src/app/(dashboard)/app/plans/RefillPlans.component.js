a5e56518123dbe18a82b2c4803e3f2375d39c719
Cinquin Andy <andy.cinquin@infotel.com>
2024-05-15 16:29:53 +0200

    🛠️ Adjust layout grid columns in RefillPlansComponent
    Increase the number of grid columns from 2 to 3 for better display of plans. Update CSS classes accordingly.

--- src/app/(dashboard)/app/plans/RefillPlans.component.js
+++ src/app/(dashboard)/app/plans/RefillPlans.component.js
@@ -73,9 +73,9 @@ export function RefillPlansComponent() {
 							anticipated usage to ensure you have enough resources available at
 							all times.
 						</p>
-						<div className="py-20">
+						<div className="pb-20">
 							<div className="mx-auto max-w-7xl px-6 lg:px-8">
-								<div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+								<div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
 									{plans.map(tier => {
 										if (tier.billingCycle) return null
 										console.log('current plan name', currentSubscription)
