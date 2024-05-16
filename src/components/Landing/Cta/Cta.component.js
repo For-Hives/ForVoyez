7eda065620527a6f306a0f0730022004b0faba89
Cinquin Andy <andy.cinquin@infotel.com>
2024-05-16 10:47:19 +0200

    ✨ Update CTA links and URLs
    Updated the CTA component links and URLs for better functionality. Removed placeholder links and added correct ones.

--- src/components/Landing/Cta/Cta.component.js
+++ src/components/Landing/Cta/Cta.component.js
@@ -15,16 +15,14 @@ export function CtaComponent() {
 					with our easy-to-use API.
 				</p>
 				<div className="mt-10 flex items-center justify-center gap-x-6">
-					{/* FIXME replace that with the correct link */}
 					<Link
-						href="#"
+						href="/app"
 						className="rounded-md bg-forvoyez_orange-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#e05d45] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-forvoyez_orange-500"
 					>
 						Generate Metadata Now
 					</Link>
-					{/* FIXME replace that with the correct link */}
 					<Link
-						href="#"
+						href="https://doc.forvoyez.com/"
 						className="text-sm font-semibold leading-6 text-slate-900"
 					>
 						Learn more <span aria-hidden="true">→</span>
