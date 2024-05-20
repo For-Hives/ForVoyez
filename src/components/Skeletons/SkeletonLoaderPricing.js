08b191cb9f63dce5bf15a8f8b5c19559a84f2d57
Cinquin Andy <andy.cinquin@infotel.com>
2024-05-20 17:32:30 +0200

    ✨ Add SkeletonLoaderPricing component for loading visuals
    Added SkeletonLoaderPricing component to display loading visuals in multiple components. This helps improve user experience by providing visual feedback during data fetching.

---
+++ src/components/Skeletons/SkeletonLoaderPricing.js
@@ -0,0 +1,24 @@
+export function SkeletonLoaderPricing(lenght = 5) {
+	return (
+		<div
+			key="custom"
+			className="animate-pulse rounded-3xl p-8 ring-1 ring-slate-200 xl:p-10"
+		>
+			<div className="my-4 h-6 w-3/4 rounded bg-gray-200"></div>
+			<div className="mb-2 h-4 w-full rounded bg-gray-200"></div>
+			<div className="my-0 h-4 w-1/4 rounded bg-gray-200"></div>
+			<div className="my-4 h-12 w-3/4 rounded bg-gray-200"></div>
+			<div className="my-4 h-4 w-1/4 rounded bg-gray-200"></div>
+			<div className="my-4 h-8 w-full rounded bg-gray-200"></div>
+
+			<ul className="mt-8 space-y-5 text-sm leading-6 text-slate-600 xl:mt-10">
+				{[...Array(11)].map((_, i) => (
+					<li key={i} className="flex gap-x-3">
+						<div className="h-4 w-5 flex-none rounded bg-gray-200"></div>
+						<div className="h-4 w-full rounded bg-gray-200"></div>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
+}
