8428234beb79d32a9cae4333ee79cbcd4368306d
Cinquin Andy <andy.cinquin@infotel.com>
2024-05-20 15:23:32 +0200

    ✨ Refactor conditional logic for plan filtering
    Simplified and commented out redundant code blocks for plan filtering based on subscription type. Added a console log statement to retrieve plans data.

--- src/app/(dashboard)/app/plans/RefillPlans.component.js
+++ src/app/(dashboard)/app/plans/RefillPlans.component.js
@@ -98,17 +98,17 @@ export function RefillPlansComponent() {
 								<div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
 									{plans.map(tier => {
 										if (tier.billingCycle) return null
-										if (
-											currentSubscription.planId === 1 ||
-											currentSubscription.planId === 2
-										) {
-											if (!tier.name.includes('Starter')) return null
-										} else if (
-											currentSubscription.planId === 3 ||
-											currentSubscription.planId === 4
-										) {
-											if (!tier.name.includes('Growth')) return null
-										}
+										// if (
+										// 	currentSubscription.planId === 1 ||
+										// 	currentSubscription.planId === 2
+										// ) {
+										// 	if (!tier.name.includes('Starter')) return null
+										// } else if (
+										// 	currentSubscription.planId === 3 ||
+										// 	currentSubscription.planId === 4
+										// ) {
+										// 	if (!tier.name.includes('Growth')) return null
+										// }
 
 										return (
 											<div
