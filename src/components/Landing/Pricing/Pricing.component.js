9bf4c7ee2ff62a7cc0ef51389c0ddb1cf3c21f60
Cinquin Andy <andy.cinquin@infotel.com>
2024-05-20 17:23:17 +0200

    ✨ Refactor pulse animation and placeholder styling
    Simplified the structure for pulse animation and placeholder styling to improve readability and maintainability.

--- src/components/Landing/Pricing/Pricing.component.js
+++ src/components/Landing/Pricing/Pricing.component.js
@@ -107,28 +107,26 @@ export function PricingComponent() {
 				<div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
 					{!(plans.length > 0)
 						? [...Array(2)].map((_, i) => (
-								<>
-									<div
-										key="custom"
-										className="animate-pulse rounded-3xl p-8 ring-1 ring-slate-200 xl:p-10"
-									>
-										<div className="my-4 h-6 w-3/4 rounded bg-gray-200"></div>
-										<div className="mb-2 h-4 w-full rounded bg-gray-200"></div>
-										<div className="my-0 h-4 w-1/4 rounded bg-gray-200"></div>
-										<div className="my-4 h-12 w-3/4 rounded bg-gray-200"></div>
-										<div className="my-4 h-4 w-1/4 rounded bg-gray-200"></div>
-										<div className="my-4 h-8 w-full rounded bg-gray-200"></div>
+								<div
+									key="custom"
+									className="animate-pulse rounded-3xl p-8 ring-1 ring-slate-200 xl:p-10"
+								>
+									<div className="my-4 h-6 w-3/4 rounded bg-gray-200"></div>
+									<div className="mb-2 h-4 w-full rounded bg-gray-200"></div>
+									<div className="my-0 h-4 w-1/4 rounded bg-gray-200"></div>
+									<div className="my-4 h-12 w-3/4 rounded bg-gray-200"></div>
+									<div className="my-4 h-4 w-1/4 rounded bg-gray-200"></div>
+									<div className="my-4 h-8 w-full rounded bg-gray-200"></div>
 
-										<ul className="mt-8 space-y-5 text-sm leading-6 text-slate-600 xl:mt-10">
-											{[...Array(11)].map((_, i) => (
-												<li key={i} className="flex gap-x-3">
-													<div className="h-4 w-5 flex-none rounded bg-gray-200"></div>
-													<div className="h-4 w-full rounded bg-gray-200"></div>
-												</li>
-											))}
-										</ul>
-									</div>
-								</>
+									<ul className="mt-8 space-y-5 text-sm leading-6 text-slate-600 xl:mt-10">
+										{[...Array(11)].map((_, i) => (
+											<li key={i} className="flex gap-x-3">
+												<div className="h-4 w-5 flex-none rounded bg-gray-200"></div>
+												<div className="h-4 w-full rounded bg-gray-200"></div>
+											</li>
+										))}
+									</ul>
+								</div>
 							))
 						: plans.map(tier => {
 								if (!isAnnually && tier.billingCycle === 'year') return null
