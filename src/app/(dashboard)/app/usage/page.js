2b6e9e38e04a336f330d4dd3f39bfff82c54f6cb
Cinquin Andy <andy.cinquin@infotel.com>
2024-05-22 14:23:07 +0200

    ✨ Refactor UI layout for API Usage page
    Updated the UI layout of the PlansPage component to improve readability and provide more detailed information about API usage.

--- src/app/(dashboard)/app/usage/page.js
+++ src/app/(dashboard)/app/usage/page.js
@@ -11,9 +11,14 @@ export const metadata = {
 
 export default function PlansPage() {
 	return (
-		<>
-			<h1 className="my-6 text-center text-2xl font-bold">API Usage</h1>
+		<div className="prose mx-auto max-w-5xl flex-auto">
+			<h1 className="text-xl font-bold text-slate-800">API Usage</h1>
+			<p className="mt-1 text-sm text-slate-600">
+				Track your API usage in real-time. Monitor your monthly credit
+				consumption and analyze trends to optimize your usage and plan ahead.
+				The chart below displays your credit usage over time.
+			</p>
 			<UsageChartComponent />
-		</>
+		</div>
 	)
 }
