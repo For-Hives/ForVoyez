b25dae30af90eeecd5cac398a078baf57c82244b
Cinquin Andy <andy.cinquin@infotel.com>
2024-05-28 11:52:23 +0200

    🛠️ Add data-testid to revoke-key-button Add data-testid attribute to the revoke key button for testing purposes.

--- src/components/Tokens/TokenModalDelete.js
+++ src/components/Tokens/TokenModalDelete.js
@@ -68,6 +68,7 @@ export default function TokenModalDelete({
 									</button>
 									<button
 										className="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+										data-testid="revoke-key-button"
 										onClick={onConfirm}
 										type="button"
 									>
