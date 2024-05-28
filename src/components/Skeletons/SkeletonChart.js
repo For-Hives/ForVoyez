75cf8d4de587d69d497b1eb86f3d6fd56e4ca0c5
Cinquin Andy <andy.cinquin@infotel.com>
2024-05-28 16:32:11 +0200

    ✨ Refactor SkeletonLoader and add data-testid props
    Updated SkeletonLoader component to accept a data-testid prop for testing purposes. Added data-testid attributes to elements in UsageChartComponent for improved testing coverage.

--- src/components/Skeletons/SkeletonChart.js
+++ src/components/Skeletons/SkeletonChart.js
@@ -1,5 +1,5 @@
-export const SkeletonLoader = () => (
-	<div className="animate-pulse">
+export const SkeletonLoader = props => (
+	<div className="animate-pulse" data-testid={props.dataTestId}>
 		<div className="h-[400px] w-full rounded-lg bg-slate-200"></div>
 	</div>
 )
