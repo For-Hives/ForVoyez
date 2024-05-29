ada0ae1e2610cb79789437dbb09776e6a92d96ff
Breval <breval2000@live.fr>
2024-05-29 13:31:27 +0200

    less buggy ?

--- src/instrumentation.js
+++ src/instrumentation.js
@@ -1,46 +1,5 @@
-import { OTLPTraceExporter as OTLPTraceExporterBrowser } from '@opentelemetry/exporter-trace-otlp-http'
-import { OTLPTraceExporter as OTLPTraceExporterNode } from '@opentelemetry/exporter-trace-otlp-http'
-import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
-import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-node'
-import { Resource } from '@opentelemetry/resources'
-import { NodeSDK } from '@opentelemetry/sdk-node'
-import { registerOTel } from '@vercel/otel'
-
-// Fonction pour créer l'exportateur OTLP basé sur l'environnement
-const createTraceExporter = () => {
-	const exporterOptions = {
-		url: 'http://captain.beta.andy-cinquin.fr:4318/v1/traces',
-	}
-
-	if (typeof window === 'undefined') {
-		// Environnement serveur
-		return new OTLPTraceExporterNode(exporterOptions)
-	} else {
-		// Environnement navigateur
-		return new OTLPTraceExporterBrowser(exporterOptions)
-	}
-}
-
-const traceExporter = createTraceExporter()
-
-export function register() {
-	if (typeof window === 'undefined') {
-		// Configuration pour l'environnement serveur
-		const sdk = new NodeSDK({
-			resource: new Resource({
-				[SemanticResourceAttributes.SERVICE_NAME]: 'your-project-name',
-			}),
-			spanProcessor: new SimpleSpanProcessor(traceExporter),
-		})
-
-		sdk.start()
-		console.info('OpenTelemetry initialized for server')
-	} else {
-		// Configuration pour l'environnement navigateur
-		registerOTel({
-			serviceName: 'your-project-name',
-			traceExporter: traceExporter,
-		})
-		console.info('OpenTelemetry initialized for browser')
+export async function register() {
+	if (process.env.NEXT_RUNTIME === 'nodejs') {
+		await import('./instrumentation.node.js')
 	}
 }
---
+++ src/instrumentation.node.js
@@ -0,0 +1,30 @@
+import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
+import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-node'
+import { Resource } from '@opentelemetry/resources'
+import { NodeSDK } from '@opentelemetry/sdk-node'
+
+// Fonction pour créer l'exportateur OTLP basé sur l'environnement
+const createTraceExporter = () => {
+	const exporterOptions = {
+		url: 'http://captain.beta.andy-cinquin.fr:4318/v1/traces',
+	}
+
+	if (typeof window === 'undefined') {
+		// Environnement serveur
+		return new OTLPTraceExporter(exporterOptions)
+	} else {
+		// Environnement navigateur
+		return new OTLPTraceExporter(exporterOptions)
+	}
+}
+
+const sdk = new NodeSDK({
+	resource: new Resource({
+		[SEMRESATTRS_SERVICE_NAME]: 'next-app',
+	}),
+	spanProcessor: new SimpleSpanProcessor(new OTLPTraceExporter()),
+	traceExporter: createTraceExporter(),
+})
+sdk.start()
+console.info('Tracing initialized')
