962eb2a1488a1f7d17025bc1f5279644ca0df1a6
Cinquin Andy <andy.cinquin@infotel.com>
2024-06-10 16:55:31 +0200

    page to layout & client to page , respect the nextjs hierarchy

---
+++ src/app/(dashboard)/app/plans/layout.js
@@ -0,0 +1,14 @@
+import ClientLogicPlans from '@/app/(dashboard)/app/plans/page'
+
+export const metadata = {
+	description:
+		"Explore ForVoyez's flexible pricing plans designed to suit your image metadata generation needs and scale with your business.",
+	alternates: {
+		canonical: '/app/plans',
+	},
+	title: 'Pricing Plans - ForVoyez',
+}
+
+export default function PlansPage() {
+	return <ClientLogicPlans />
+}
