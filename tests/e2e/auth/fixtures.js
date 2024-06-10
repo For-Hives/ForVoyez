d9075065adf55500d69f57e6c162f04fd6f074f0
Cinquin Andy <andy.cinquin@infotel.com>
2024-06-10 12:01:44 +0200

    ✨ Update authentication process based on mode
    Refactor authentication to support different user modes for testing. Include subscribed user credentials when in subscribed mode.

--- tests/e2e/auth/fixtures.js
+++ tests/e2e/auth/fixtures.js
@@ -8,13 +8,15 @@ require('dotenv').config()
 const NEXT_PUBLIC_URL = getNextPublicUrl()
 const TEST_EMAIL = process.env.TEST_EMAIL
 const TEST_PASSWORD = process.env.TEST_PASSWORD
+const SUBSCRIBED_USER_EMAIL = process.env.SUBSCRIBED_USER_EMAIL
+const SUBSCRIBED_USER_PASSWORD = process.env.SUBSCRIBED_USER_PASSWORD
 
 module.exports = {
 	...require('@playwright/test'),
 	test: baseTest.extend({
 		// Authenticate once per worker with a worker-scoped fixture.
 		workerStorageState: [
-			async ({ browser }, use) => {
+			async ({ browser }, use, testInfo) => {
 				// Use parallelIndex as a unique identifier for each worker.
 				const id = baseTest.info().parallelIndex
 				const fileName = path.resolve(
@@ -28,11 +30,23 @@ module.exports = {
 					return
 				}
 
+				// Check if subscribed mode is enabled
+				const isSubscribedMode = testInfo.project.name.includes('subscribed')
+
 				// Important: make sure we authenticate in a clean environment by unsetting storage state.
 				const page = await browser.newPage({ storageState: undefined })
 
-				// Perform authentication steps.
-				await signIn(page, NEXT_PUBLIC_URL, TEST_EMAIL, TEST_PASSWORD)
+				// Perform authentication steps based on the mode
+				if (isSubscribedMode) {
+					await signIn(
+						page,
+						NEXT_PUBLIC_URL,
+						SUBSCRIBED_USER_EMAIL,
+						SUBSCRIBED_USER_PASSWORD
+					)
+				} else {
+					await signIn(page, NEXT_PUBLIC_URL, TEST_EMAIL, TEST_PASSWORD)
+				}
 
 				await page.context().storageState({ path: fileName })
 				await page.close()
