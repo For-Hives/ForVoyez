f8ddadd364c9ec13e50b872322aea3d40468d318
Cinquin Andy <andy.cinquin@infotel.com>
2024-06-13 13:38:42 +0200

    🔧 Add debug log for plans in getPlans function
    Add console.info to log plans before returning them.

--- src/services/database.service.js
+++ src/services/database.service.js
@@ -23,6 +23,7 @@ export async function getPlans(filter = null) {
 	if (filter) {
 		return plans.filter(plan => plan.billingCycle === filter)
 	}
+	console.info('********************** Plans:', plans)
 	return plans
 }
 
