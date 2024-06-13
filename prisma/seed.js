cfbd793e404fa69a6565bca6e41495740304e78f
Cinquin Andy <andy.cinquin@infotel.com>
2024-06-13 15:26:24 +0200

    ✨ Update usage data with previous and current credits
    Added previous and current credit values for each usage entry in the code. This change ensures accurate tracking of credit changes over time.

--- prisma/seed.js
+++ prisma/seed.js
@@ -224,26 +224,6 @@ const subscriptionData = [
 	},
 ]
 
-,5990
-,5993
-,5995
-,5988
-,5984
-,5983
-,5987
-,3997
-,5982
-,4996
-,3991
-,5989
-,5994
-,5986
-,1999
-,4992
-,2998
-,5981
-,5985
-
 // token (not need)
 // usage data
 const usageData = [
@@ -252,190 +232,190 @@ const usageData = [
 		id: '5e5615c2-aa0b-4d1c-98bb-cd2ad36145d7',
 		userId: 'user_2h6GYOvlTOlyK2w0OaqnyRLf8TW',
 		reason: 'describe from PlaygroundAction',
+		previousCredits: 5990,
+		currentCredits: 5989,
 		api: 'describe',
 		used: -1,
-		currentCredits: 5989,
-		previousCredits: 5990
 	},
 	{
 		usedAt: new Date('2024-06-03 19:15:26.938'),
 		id: '39bee856-5f74-4f57-9f89-96c25abffb69',
 		userId: 'user_2h6GYOvlTOlyK2w0OaqnyRLf8TW',
 		reason: 'describe from PlaygroundAction',
+		previousCredits: 5993,
+		currentCredits: 5992,
 		api: 'describe',
 		used: -1,
-		currentCredits: 5992,
-		previousCredits: 5993
 	},
 	{
 		usedAt: new Date('2024-06-03 19:17:39.750'),
 		id: '6d27e371-04b9-4259-832a-91d301a6cba9',
 		userId: 'user_2h6GYOvlTOlyK2w0OaqnyRLf8TW',
 		reason: 'describe from PlaygroundAction',
+		previousCredits: 5995,
+		currentCredits: 5994,
 		api: 'describe',
 		used: -1,
-		currentCredits: 5994,
-		previousCredits: 5995
 	},
 	{
 		usedAt: new Date('2024-06-04 07:53:34.634'),
 		id: '387e0259-d360-4827-b21c-c6a2a4dc6dac',
 		userId: 'user_2h6GYOvlTOlyK2w0OaqnyRLf8TW',
 		reason: 'describe from PlaygroundAction',
+		previousCredits: 5988,
+		currentCredits: 5987,
 		api: 'describe',
 		used: -1,
-		currentCredits: 5987,
-		previousCredits: 5988
 	},
 	{
 		usedAt: new Date('2024-06-04 09:05:33.478'),
 		id: 'e75bbb67-4a77-400f-81cf-4330ad080cb4',
 		userId: 'user_2h6GYOvlTOlyK2w0OaqnyRLf8TW',
 		reason: 'describe from PlaygroundAction',
+		previousCredits: 5984,
+		currentCredits: 5983,
 		api: 'describe',
 		used: -1,
-		currentCredits: 5983,
-		previousCredits: 5984
 	},
 	{
 		usedAt: new Date('2024-06-04 09:21:54.728'),
 		id: '9caebdf9-d179-4c98-b7d8-49f555ccea5a',
 		userId: 'user_2h6GYOvlTOlyK2w0OaqnyRLf8TW',
 		reason: 'describe from PlaygroundAction',
+		previousCredits: 5983,
+		currentCredits: 5982,
 		api: 'describe',
 		used: -1,
-		currentCredits: 5982,
-		previousCredits: 5983
 	},
 	{
 		usedAt: new Date('2024-06-04 09:29:41.551'),
 		id: '34a59344-ffaf-4245-ae05-33065bf0f850',
 		userId: 'user_2h6GYOvlTOlyK2w0OaqnyRLf8TW',
 		reason: 'describe from PlaygroundAction',
+		previousCredits: 5987,
+		currentCredits: 5986,
 		api: 'describe',
 		used: -1,
-		currentCredits: 5986,
-		previousCredits: 5987
 	},
 	{
 		usedAt: new Date('2024-06-04 09:32:59.326'),
 		id: '76d927a8-8597-4c20-b054-2e2ccd306bc5',
 		userId: 'user_2h6GYOvlTOlyK2w0OaqnyRLf8TW',
 		reason: 'describe from PlaygroundAction',
+		previousCredits: 3997,
+		currentCredits: 5997,
 		api: 'describe',
 		used: -1,
-		currentCredits: 5997,
-		previousCredits: 3997
 	},
 	{
 		usedAt: new Date('2024-06-10 12:32:42.255'),
 		id: '8fa6b530-9530-495a-9af5-b424464c13b4',
 		userId: 'user_2h6GYOvlTOlyK2w0OaqnyRLf8TW',
 		reason: 'describe from PlaygroundAction',
+		previousCredits: 5982,
+		currentCredits: 5981,
 		api: 'describe',
 		used: -1,
-		currentCredits: 5981,
-		previousCredits: 5982
 	},
 	{
 		usedAt: new Date('2024-06-10 12:34:55.050'),
 		id: 'c3885879-0a55-4ad3-83b2-3d712cfa6028',
 		userId: 'user_2h6GYOvlTOlyK2w0OaqnyRLf8TW',
 		reason: 'describe from PlaygroundAction',
+		previousCredits: 4996,
+		currentCredits: 5995,
 		api: 'describe',
 		used: -1,
-		currentCredits: 5995,
-		previousCredits: 4996
 	},
 	{
 		usedAt: new Date('2024-06-01 15:19:00.943'),
 		id: 'c769c272-7ef7-4c64-b18a-b77399b4cf1a',
 		userId: 'user_2h6GYOvlTOlyK2w0OaqnyRLf8TW',
 		reason: 'describe from PlaygroundAction',
+		previousCredits: 3991,
+		currentCredits: 5990,
 		api: 'describe',
 		used: -1,
-		currentCredits: 5990,
-		previousCredits: 3991
 	},
 	{
 		usedAt: new Date('2024-05-07 15:19:00.943'),
 		id: '49bbb203-5fca-4561-97cc-e5f48764a437',
 		userId: 'user_2h6GYOvlTOlyK2w0OaqnyRLf8TW',
 		reason: 'describe from PlaygroundAction',
+		previousCredits: 5989,
+		currentCredits: 5988,
 		api: 'describe',
 		used: -1,
-		currentCredits: 5988,
-		previousCredits: 5989
 	},
 	{
 		usedAt: new Date('2024-05-24 15:19:00.943'),
 		id: '5b9a56e4-d213-4623-96ae-a8535f1403bd',
 		userId: 'user_2h6GYOvlTOlyK2w0OaqnyRLf8TW',
 		reason: 'describe from PlaygroundAction',
+		previousCredits: 5994,
+		currentCredits: 5993,
 		api: 'describe',
 		used: -1,
-		currentCredits: 5993,
-		previousCredits: 5994
 	},
 	{
 		usedAt: new Date('2024-06-03 18:32:32.839'),
 		id: '2a358a01-3bd3-4968-a70d-13b02eea1215',
 		userId: 'user_2h6GYOvlTOlyK2w0OaqnyRLf8TW',
 		reason: 'describe from PlaygroundAction',
+		previousCredits: 5986,
+		currentCredits: 5985,
 		api: 'describe',
 		used: -1,
-		currentCredits: 5985,
-		previousCredits: 5986
 	},
 	{
 		usedAt: new Date('2024-05-03 15:19:00.943'),
 		id: '4b2e182d-abb8-4aea-93a2-d3f61e7ececd',
 		userId: 'user_2h6GYOvlTOlyK2w0OaqnyRLf8TW',
 		reason: 'describe from PlaygroundAction',
+		previousCredits: 1999,
+		currentCredits: 5999,
 		api: 'describe',
 		used: -1,
-		currentCredits: 5999,
-		previousCredits: 1999
 	},
 	{
 		usedAt: new Date('2024-05-02 15:19:00.943'),
 		id: '64772419-1136-40f3-9a45-dd6c09e3e867',
 		userId: 'user_2h6GYOvlTOlyK2w0OaqnyRLf8TW',
 		reason: 'describe from PlaygroundAction',
+		previousCredits: 4992,
+		currentCredits: 5991,
 		api: 'describe',
 		used: -1,
-		currentCredits: 5991,
-		previousCredits: 4992
 	},
 	{
 		usedAt: new Date('2024-06-02 18:32:07.703'),
 		id: 'dae73eac-fb1f-48d9-82d3-58026377869e',
 		userId: 'user_2h6GYOvlTOlyK2w0OaqnyRLf8TW',
 		reason: 'describe from PlaygroundAction',
+		previousCredits: 2998,
+		currentCredits: 5998,
 		api: 'describe',
 		used: -1,
-		currentCredits: 5998,
-		previousCredits: 2998
 	},
 	{
 		usedAt: new Date('2024-05-06 15:19:00.943'),
 		id: 'f64f1288-459b-4847-be30-a03161cd7e11',
 		userId: 'user_2h6GYOvlTOlyK2w0OaqnyRLf8TW',
 		reason: 'describe from PlaygroundAction',
+		previousCredits: 5981,
+		currentCredits: 5980,
 		api: 'describe',
 		used: -1,
-		currentCredits: 5980,
-		previousCredits: 5981
 	},
 	{
 		usedAt: new Date('2024-05-01 15:19:00.943'),
 		id: '4cf7167f-52f9-4680-96c4-251e8380a620',
 		userId: 'user_2h6GYOvlTOlyK2w0OaqnyRLf8TW',
 		reason: 'describe from PlaygroundAction',
+		previousCredits: 5985,
+		currentCredits: 5984,
 		api: 'describe',
 		used: -1,
-		currentCredits: 5984,
-		previousCredits: 5985
 	},
 ]
 
