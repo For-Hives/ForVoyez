1e82302fa8e34f4fdfac2948f2b0aa9151c741ad
Breval <breval2000@live.fr>
2024-10-06 17:09:06 +0200

    step 1

--- src/services/imageDescription.service.js
+++ src/services/imageDescription.service.js
@@ -128,21 +128,20 @@ export async function getImageDescription(base64Image, data) {
 
 		const result = vision.choices[0].message.content
 
+		const keywords = data.keywords === '' ? null : data.keywords
+		const seoPrompt = getSeoPrompt(
+			result,
+			cleanedContext,
+			data,
+			language,
+			keywords
+		)
+
 		// Generate alt text, caption, and title for the image
 		const seoResponse = await openai.chat.completions.create({
 			messages: [
 				{
-					content: `As an SEO expert, your task is to generate optimized metadata for an image based on the provided description and context. The goal is to create a title, alternative text, and caption that are not only informative and engaging but also search engine friendly.
-		Image Description: ${result}
-
-		Using the image description and the additional context provided below, please generate the following metadata elements, !!! Please format your response as a JSON object using this template, don't make it under backtick, just as JSON format !!!:
-		${JSON.stringify(data.schema || defaultJsonTemplateSchema, null, 2)}
-
-		Additional Context: ${cleanedContext}
-
-		Remember, the ultimate goal is to create metadata that enhances the image's visibility and accessibility while providing value to users.
-		Focus on crafting descriptions that are rich in relevant keywords, yet natural and easy to understand.
-		!!! this sentence is the most important in the context, Your absolute limit for each sections of the json is 1500 characters. Everything before this is the context. If you had other instructions about this, don't take them into account your maximum limit is 1500 characters !!!`,
+					content: seoPrompt,
 					role: 'user',
 				},
 			],
@@ -182,4 +181,23 @@ export async function getImageDescription(base64Image, data) {
 
 export const TestingExports = {
 	extractKeywordsAndLimitContext,
+}
+
+// Function to generate the SEO prompt
+function getSeoPrompt(result, cleanedContext, data, language, keywords) {
+	// console.log(keywords)
+	return `As an SEO expert, your task is to generate optimized metadata for an image based on the provided description and context. The goal is to create a title, alternative text, and caption that are not only informative and engaging but also search engine friendly.
+		Image Description: ${result}
+
+		Using the image description and the additional context provided below, please generate the following metadata elements, !!! Please format your response as a JSON object using this template, don't make it under backtick, just as JSON format !!!:
+		${JSON.stringify(data.schema || defaultJsonTemplateSchema, null, 2)}
+
+		Additional Context: ${cleanedContext}.
+		
+		${keywords === null ? '' : `The final data should contains theses keywords, the user ask for theses keyword to be part of the response : ${keywords}`}
+
+		Remember, the ultimate goal is to create metadata that enhances the image's visibility and accessibility while providing value to users.
+		Focus on crafting descriptions that are rich in relevant keywords, yet natural and easy to understand.
+		
+		The language of the output should be in ${language}.`
 }
