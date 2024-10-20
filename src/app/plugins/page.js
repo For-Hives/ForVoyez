2e8660d7d05fdee69e505c53851a6f59812a8fac
Cinquin Andy <cinquin.andy@gmail.com>
2024-10-20 18:59:30 +0200

    feat(desing-wp-plugin): features section done

--- src/app/plugins/page.js
+++ src/app/plugins/page.js
@@ -1,15 +1,7 @@
+import { FeatureHighlightsComponent } from '@/components/Plugins/FeatureHighlights.component'
 import { HeroSectionComponent } from '@/components/Plugins/HeroSection.component'
 import { NavbarComponent } from '@/components/Navbar.component'
 import { FooterComponent } from '@/components/Footer.component'
-// import { FeatureHighlightsPlugin } from '@/components/Plugin/FeatureHighlightsPlugin'
-// import { HowItWorksPlugin } from '@/components/Plugin/HowItWorksPlugin'
-// import { DetailedBenefitsPlugin } from '@/components/Plugin/DetailedBenefitsPlugin'
-// import { DemoPlugin } from '@/components/Plugin/DemoPlugin'
-// import { TestimonialsPlugin } from '@/components/Plugin/TestimonialsPlugin'
-// import { PricingPlugin } from '@/components/Plugin/PricingPlugin'
-// import { FaqPlugin } from '@/components/Plugin/FaqPlugin'
-// import { CtaPlugin } from '@/components/Plugin/CtaPlugin'
-// import { SupportDocPlugin } from '@/components/Plugin/SupportDocPlugin'
 
 export default function WordPressPluginPage() {
 	return (
@@ -17,7 +9,7 @@ export default function WordPressPluginPage() {
 			<NavbarComponent />
 			<main>
 				<HeroSectionComponent />
-				{/*<FeatureHighlightsPlugin />*/}
+				<FeatureHighlightsComponent />
 				{/*<HowItWorksPlugin />*/}
 				{/*<DetailedBenefitsPlugin />*/}
 				{/*<DemoPlugin />*/}
