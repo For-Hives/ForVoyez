761f3a09668404aa38984409edee1010482c4a4d
Breval <breval2000@live.fr>
2024-10-31 14:47:45 +0100

    test
    The code change removes an unnecessary condition related to NODE_ENV in the register function.

--- src/instrumentation.js
+++ src/instrumentation.js
@@ -1,7 +1,8 @@
 export async function register() {
 	if (
-		process.env.NEXT_RUNTIME === 'nodejs' &&
-		process.env.NODE_ENV === 'production'
+		process.env.NEXT_RUNTIME === 'nodejs'
+		// &&
+		// process.env.NODE_ENV === 'production'
 	) {
 		console.info('Instrumentation is enabled, your are in production mode')
 		await import('./instrumentation.node.js')
