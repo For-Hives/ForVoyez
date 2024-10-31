d4b30d37dd4f439c976a97844a36e4a4825f4e41
Breval <breval2000@live.fr>
2024-10-31 14:28:05 +0100

    feat: Improve production mode instrumentation message
    Add a console log for production mode in the register function.

--- src/instrumentation.js
+++ src/instrumentation.js
@@ -1,5 +1,9 @@
 export async function register() {
-	if (process.env.NEXT_RUNTIME === 'nodejs') {
+	if (
+		process.env.NEXT_RUNTIME === 'nodejs' &&
+		process.env.NODE_ENV === 'production'
+	) {
+		console.info('Instrumentation is enabled, your are in production mode')
 		await import('./instrumentation.node.js')
 	}
 }
