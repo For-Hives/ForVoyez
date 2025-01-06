10d185a1c4406157fb602f93071258eb8588f0fb
Breval <breval2000@live.fr>
2025-01-06 22:19:15 +0100

    feat: upgrade nextjs15
    chore: update dependencies and scripts
    
    - Changed dev script to use turbopack.
    - Updated @clerk/nextjs from 5.7.5 to 6.9.6.
    - Upgraded next from 14.2.22 to 15.1.3.
    - Added new devDependencies for testing and typography.
    - Adjusted eslint-config-next version to match next upgrade.
    ```

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,7 +1,7 @@
 import { cookies } from 'next/headers'
 
 export async function GET() {
-	const cookieStore = cookies()
+	const cookieStore = await cookies()
 	const token = cookieStore.get('token')
 
 	return new Response('Hello, Next.js!', {
